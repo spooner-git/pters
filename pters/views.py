0c56f126e3bc223baa743cdcf230e8d7b025e4bb
DESKTOP-2BH3194\Skkim <skkim.ku@gmail.com>
2017-08-02 15:42:35 +0900

    Main page 생성

--- pters/views.py
+++ pters/views.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = 'main_trainee.html'
+    template_name = 'month_cal.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
