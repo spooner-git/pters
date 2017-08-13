ba62c6c4b2af193b00f42f12c0ee0d0261f3f60d
DESKTOP-2BH3194\Skkim <skkim.ku@gmail.com>
2017-08-13 22:27:36 +0900

    플로팅버튼 애니메이션추가

--- static/common/js/pters.day.cal.js
+++ static/common/js/pters.day.cal.js
@@ -16,11 +16,13 @@ $(document).ready(function(){
 	$('#float_btn').click(function(){
 		if($('#shade').css('z-index')<0){
 			$('#shade').css({'background-color':'black','z-index':'8'});
-			$('#float_inner1').animate({'opacity':'0.7','top':'80%'});
-			$('#float_inner2').animate({'opacity':'0.7','top':'70%'});
+			$('#float_inner1').animate({'opacity':'0.7','bottom':'85px'},120);
+			$('#float_inner2').animate({'opacity':'0.7','bottom':'145px'},120);
+			$('#float_btn img').addClass('rotate_btn');
 		}else{
 			$('#shade').css({'background-color':'white','z-index':'-1'});
-			$('#float_inner1,#float_inner2').animate({'opacity':'0','top':'90%'})
+			$('#float_inner1,#float_inner2').animate({'opacity':'0','bottom':'25px'},10);
+			$('#float_btn img').removeClass('rotate_btn');
 		}
 	});
 	//플로팅 버튼
