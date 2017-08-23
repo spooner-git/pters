c0a8bb8d1227d9b122c343cdb8e1e1246b7a70ba
savekhg <savekhg@gmail.com>
2017-08-23 20:41:51 +0900

    recover

---
+++ trainee/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        return context
+
