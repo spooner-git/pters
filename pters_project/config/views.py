e25bcd03f761f7080e9433d70fe9850edc1680a5
savekhg <savekhg@gmail.com>
2017-09-23 00:20:00 +0900

    db 아이디 체크 테스트 및 기본 폴더 구조 변경

--- pters_project/config/views.py
+++ pters_project/config/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from mobile.models import MemberTb
 
 
 class HomeView(TemplateView):
@@ -9,6 +10,14 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
+        context = get_login_member_info(context)
 
         return context
+
+
+def get_login_member_info(context):
+    member_detail = MemberTb.objects.filter(member_id='TB05000002')
+    context['login_member'] = list(member_detail)[0]
+
+    return context
 
--- pters_project/schedule/views.py
+++ pters_project/schedule/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
-
+from config.views import get_login_member_info
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        context = get_login_member_info(context)
 
         return context
 
@@ -18,6 +19,7 @@ class CalDayView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(CalDayView, self).get_context_data(**kwargs)
+        context = get_login_member_info(context)
 
         return context
 
@@ -27,5 +29,6 @@ class CalMonthView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(CalMonthView, self).get_context_data(**kwargs)
+        context = get_login_member_info(context)
 
         return context
