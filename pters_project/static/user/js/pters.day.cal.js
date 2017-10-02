61c800adbb9b24aa017319cbe7c44559b42d173f
SKKIM <skkim.ku@gmail.com>
2017-10-02 15:29:05 +0900

    전체적으로 css 및 js 정리

--- pters_project/static/user/js/pters.day.cal.js
+++ pters_project/static/user/js/pters.day.cal.js
@@ -18,11 +18,11 @@ $(document).ready(function(){
 			$('#shade').css({'background-color':'black','z-index':'8'});
 			$('#float_inner1').animate({'opacity':'0.7','bottom':'85px'},120);
 			$('#float_inner2').animate({'opacity':'0.7','bottom':'145px'},120);
-			$('#float_btn img').addClass('rotate_btn');
+			$('#float_btn').addClass('rotate_btn');
 		}else{
 			$('#shade').css({'background-color':'white','z-index':'-1'});
 			$('#float_inner1,#float_inner2').animate({'opacity':'0','bottom':'25px'},10);
-			$('#float_btn img').removeClass('rotate_btn');
+			$('#float_btn').removeClass('rotate_btn');
 		}
 	});
 	//플로팅 버튼
