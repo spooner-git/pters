f70478be2cc17850909fe32409832affcea2b023
savekhg <savekhg@gmail.com>
2017-10-06 03:39:25 +0900

    첫 페이지 연동 - 오늘의 일정, 종료예정 회원

--- pters_project/config/views.py
+++ pters_project/config/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 
@@ -6,6 +7,7 @@ from django.views.generic import TemplateView
 from login.models import MemberTb
 from django.contrib.auth.mixins import LoginRequiredMixin
 from config.settings import LOGIN_URL
+from schedule.models import LectureScheduleTb
 from trainee.models import LectureTb
 from trainer.models import ClassTb
 
@@ -29,6 +31,24 @@ class HomeView(LoginRequiredMixin, TemplateView):
 
         if error is None :
             context['trainer_member_count'] = LectureTb.objects.filter(class_tb_id=trainer_class.class_id).count()
+            context['trainer_end_member_count'] = LectureTb.objects.filter(class_tb_id=trainer_class.class_id,
+                                                                           lecture_count__gte=1,
+                                                                           lecture_count__lte=3).count()
+
+        today_time = datetime.datetime.now()
+        today_start_time = today_time.strftime('%Y-%m-%d 00:00:00.000000')
+        today_end_time = today_time.strftime('%Y-%m-%d 23:59:59.999999')
+        sum_val = 0;
+        if error is None:
+            month_lecture_data = LectureTb.objects.filter(class_tb_id=trainer_class.class_id)
+
+            for lecture in month_lecture_data:
+                sum_val = sum_val+LectureScheduleTb.objects.filter(lecture_tb=lecture.lecture_id,
+                                                                   start_dt__gte=today_start_time,
+                                                                   start_dt__lte=today_end_time,
+                                                                   en_dis_type='1',use='1').count()
+
+        context['today_schedule_val'] = sum_val
 
         return context
 
--- pters_project/static/user/js/main/main_trainer.js
+++ pters_project/static/user/js/main/main_trainer.js
@@ -8,22 +8,22 @@ $(document).ready(function(){
  	var currentDay = new Array('일','월','화','수','목','금','토')
 
  	var plan = "오늘의 수업 일정";
- 	var num = 5;
+ 	//var num = 5;
  	var gun = "건"
 
  	//var totalMember = 38;
  	var requestNum = 3;
- 	var finishMember = 8;
+ 	//var finishMember = 8;
 
 	$('.center_box_day p').text(currentDate+','+currentDay[currentDayinfo]); //일, 요일 표시
 	$('.center_box_monthyear p').text(currentMonth+1+'월 '+currentYear+'년'); //월, 연도 표시
 
 	$('.center_box_plan p').text(plan); //오늘의 수업 일정
-	$('.center_box_gun a').text(num); // 몇 건 있는지
+	//$('.center_box_gun a').text(num); // 몇 건 있는지
 
 	//$('#total_member').text(totalMember); //총 회원수
 	$('#request_num').text(requestNum); //일정 변경 수
-	$('#finish_member').text(finishMember); //종료 임박 회원수
+	//$('#finish_member').text(finishMember); //종료 임박 회원수
 });
 
 //2017.8.3변경 내역
