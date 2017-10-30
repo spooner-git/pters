9283202d3d0ece27e544cac9d72b64a5bc7d0e7d
savekhg <savekhg@gmail.com>
2017-10-31 01:05:26 +0900

    alarm 등록시간 전달 및 삭제기능 구현

--- pters_project/trainer/urls.py
+++ pters_project/trainer/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     url(r'^member_registration/$', views.member_registration, name='member_registration'),
     url(r'^trainer_login/$', views.LogInTrainerView.as_view(), name='trainer_login'),
     url(r'^alarm/$', views.AlarmView.as_view(), name='alarm'),
+    url(r'^alarm_delete/$', views.alarm_delete_logic, name='alarm_delete'),
     url(r'^cal_day/$', views.CalDayView.as_view(), name='cal_day'),
     url(r'^cal_week/$', views.CalWeekView.as_view(), name='cal_week'),
     url(r'^daily_pt_delete/$', views.daily_pt_delete, name='daily_pt_delete'),
