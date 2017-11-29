b1a65808c48a7f068c59536cd2c6135af13e183d
SKKIM <skkim.ku@gmail.com>
2017-11-29 17:35:04 +0900

    회원 마이페이지 그래프 2016년 2017년

--- pters_project/static/user/js/pters.trainee.mypage.js
+++ pters_project/static/user/js/pters.trainee.mypage.js
@@ -11,15 +11,37 @@ year를 4로 나누었을때 0이 되는 year에는 2월을 29일로 계산
 
 $(document).ready(function(){
 
-	var countPerMonth = [1,10,20,15,25,20,15,0,0,10,25,20] //12달 순서대로 각달의 PT 수업 갯수
+	var currentYearhistory = [1,9,29,27,22,26,18,11,0,16,19,0] //12달 순서대로 각달의 PT 수업 갯수
+	var lastYearhistory = [0,0,0,0,0,0,0,0,0,12,18,12]
 
-	graphSet(36,30);
-	dataSet();
+	var graphVerticalLength = 30;
+	var graphHorizontalLength = 36
+	graphSet(graphHorizontalLength,graphVerticalLength); //그래프 수직,수평 셋팅
+	dataSet(currentYearhistory); //그래프에 데이터 채우기
+	graphUnit(5); //그래프에 일정 단위마다 빨간선 긋기
+	
 
 
+	var date = new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth()+1
 
+	$('#yy').text(year);
+	arrowSet();
 
+	$('#leftArrow').click(function(){
+		$('td').removeClass('graphcoloring')
+		$('#yy').text(year-1);
+		arrowSet();
+		dataSet(lastYearhistory);
+	})
 
+	$('#rightArrow').click(function(){
+		$('td').removeClass('graphcoloring')
+		$('#yy').text(year);
+		arrowSet();
+		dataSet(currentYearhistory);
+	})
 
 
 	function graphSet(horizontal,vertical){ //그래프 눈금 셋팅 horizontal*vertical 갯수 타일
@@ -37,20 +59,38 @@ $(document).ready(function(){
 			}
 			var trSum = tr + tds + trEnd;
 			var trSums = trSums + trSum
-			console.log(trSums)
 		}
 		var tableSet = table + trSums + tableEnd
 		loc.append(tableSet)
 	}
 
 //graphcoloring  2,5,8,11,.... 2+(3*i)
-	function dataSet(){
+	function dataSet(year){
 		for(var i=0; i<12; i++){
-			var thismonth = countPerMonth[i]
+			var thismonth = year[i]
 			var loc = 2+(3*i)+"_"
 			for(var j=0; j<=thismonth;j++){
 				$('#'+loc+j).addClass('graphcoloring')
 			}
+		}
+	}
+
+	function graphUnit(unit){
+		var len = graphHorizontalLength;
+		for(var i=1; i<=len; i++){
+			for(var j=0; j<graphVerticalLength/unit ;j++){
+				$('#'+i+'_'+unit*j).addClass('graphunit')
+			}
+		}
+	}
+
+	function arrowSet(){
+		if($('#yy').text()==year){
+			$('#rightArrow').hide()
+			$('#leftArrow').show()
+		}else{
+			$('#rightArrow').show()
+			$('#leftArrow').hide()
 		}
 	}
 
