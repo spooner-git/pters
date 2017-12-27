9856d34e854b89093e14913a5dc268c65ca65905
SKKIM <skkim.ku@gmail.com>
2017-12-27 11:28:17 +0900

    일정,회원등록 sending시 loading 이미지 출력

--- pters_project/static/user/js/trainer/pters.trainer.manage.member.add.js
+++ pters_project/static/user/js/trainer/pters.trainer.manage.member.add.js
@@ -316,15 +316,26 @@ $(document).ready(function(){
             console.log('ajax')
            
                  //ajax 회원정보 입력된 데이터 송신
+                 
                  $.ajax({
                     url:'/trainer/member_registration/',
                     type:'POST',
                     data:$form.serialize(),
 
+                    beforeSend:function(){
+                      $('html').css("cursor","wait")
+                      $('#upbutton-check img').attr('src','/static/user/res/ajax/loading.gif')
+                    },
+
                     //보내기후 팝업창 닫기
                     complete:function(){
                       closePopup()
-                      console.log('complete')
+                       if($('body').width()<600){
+                          $('#page_managemember').show();
+                        }
+                        $('html').css("cursor","auto")
+                        $('#upbutton-check img').attr('src','/static/user/res/ptadd/btn-complete.png')
+                      alert('complete: 회원 정상 등록')
                       },
 
                     //통신성공시 처리
@@ -334,7 +345,7 @@ $(document).ready(function(){
 
                     //통신 실패시 처리
                     error:function(){
-                      console.log("error")
+                      alert("error: 이미 등록된 회원인지 확인")
                     },
                  })
             
