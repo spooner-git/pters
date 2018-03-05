eeb1640e050e25f9066ef253728e2870e690b40b
DESKTOP-Q3N0A3M\skkim <skkim.ku@gmail.com>
2018-03-05 17:28:05 +0900

    off반복일정 요약

--- pters_project/static/user/js/trainer/pters.trainer.cal.week.js
+++ pters_project/static/user/js/trainer/pters.trainer.cal.week.js
@@ -11,7 +11,7 @@ year를 4로 나누었을때 0이 되는 year에는 2월을 29일로 계산
 
 $(document).ready(function(){
 
-	setInterval(function(){ajaxClassTime()},60000)
+	//setInterval(function(){ajaxClassTime()},60000) 자동 ajax 새로고침(일정가져오기)
 
 	$('#float_inner1, .ymdText-pc-add-pt').click(function(){ //PT추가버튼
 		scrollToDom($('#calendar'))
