270ba33a7db0fe93899bf753e3cefbb30ed3bcfc
DESKTOP-Q3N0A3M\skkim <skkim.ku@gmail.com>
2018-03-09 13:33:59 +0900

    예약허용시간대 설정

--- pters_project/static/user/js/trainer/pters.trainer.setting.reserve.js
+++ pters_project/static/user/js/trainer/pters.trainer.setting.reserve.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 
      var select_all_check = false;
 
+     /*
      if(Options.rsvDisableSet=="on"){
         $('#switch1').find('.switchball').removeClass('switchoff').addClass('switchon')
         $('#switch1').find('.switchback').addClass('switchon-back')
@@ -39,6 +40,7 @@ $(document).ready(function(){
         $("#morningdaySelect2 button").addClass("dropdown_selected").text("오전").val("오전");
         $("#hourSelect2 button").addClass("dropdown_selected").text(Options.stoptimeStart+"시").val(Options.stoptimeStart);
      }
+     */
 
      $('#uptext').text("예약 허용 시간대 설정")
      $('.pters_switch').click(function(){
@@ -54,63 +56,80 @@ $(document).ready(function(){
      $('#switch1').click(function(){
         if($(this).find('.switchball').hasClass('switchoff')){
           $('#memberDisableSetting').hide('fast','swing')
-          Options.rsvDisableSet = 'off'
-          console.log(Options.rsvDisableSet)
+          $('#id_setting_member_reserve_time_prohibition').val(0)
+
         }else{
           $('#memberDisableSetting').show('fast','swing')
-          Options.rsvDisableSet = 'on'
-          console.log(Options.rsvDisableSet)
+          $('#id_setting_member_reserve_time_prohibition').val($('#timeSelect button').val())
         }
      })
 
       $('#switch2').click(function(){
         if($(this).find('.switchball').hasClass('switchoff')){
-          Options.reserve = 'disable'
-          console.log(Options.reserve)
+          $('#id_setting_member_reserve_prohibition').val(0)
         }else{
-          Options.reserve = 'enable'
-          console.log(Options.reserve)
+          $('#id_setting_member_reserve_prohibition').val(1)
         }
       })
 
+      function make_available_date_format(){
+          var startmorningday = $("#morningdaySelect button").val()
+          var endmorningday = $('#morningdaySelect2 button').val()
+          var starthour = $('#hourSelect button').val()
+          var endhour = $('#hourSelect2 button').val()
+          if(startmorningday == "오후"){
+            var starthour = Number(starthour) + 12 +':00'
+            if(starthour == "24:00"){
+              var starthour = "23:59"
+            }
+          }else if(startmorningday == "오전"){
+            var starthour = Number(starthour)+':00'
+          }
+          if(endmorningday == "오후"){
+            var endhour = Number(endhour) + 12 + ':00'
+            if(endhour=="24:00"){
+              var endhour = "23:59"
+            }
+          }else if(endmorningday == "오전"){
+            var endhour = Number(endhour) + ':00'
+          }
+          var availableTime = starthour+'-'+endhour
+          return availableTime
+      }
+
       $("#ulMorningDay li a").click(function(){
             $("#morningdaySelect button").addClass("dropdown_selected").text($(this).text()).val($(this).text())
+            var availableTime = make_available_date_format()
+            $('#id_setting_member_available_time').val(availableTime)
             check_dropdown_selected();
       });
 
 
       $("#ulMorningDay2 li a").click(function(){
             $("#morningdaySelect2 button").addClass("dropdown_selected").text($(this).text()).val($(this).text());
+            var availableTime = make_available_date_format()
+            $('#id_setting_member_available_time').val(availableTime)
             check_dropdown_selected();
       });
 
       $("#ulTimes li a").click(function(){
-            $("#hourSelect button").addClass("dropdown_selected").text($(this).text()).val($(this).text());
+            $("#hourSelect button").addClass("dropdown_selected").text($(this).text()).val($(this).attr('data-time'));
+            var availableTime = make_available_date_format()
+            $('#id_setting_member_available_time').val(availableTime)
             check_dropdown_selected();
-
-            if($("#morningdaySelect button").val()=="오전"){
-              Options.stoptimeEnd = $(this).attr('data-dur');
-            }else if($("#morningdaySelect button").val()=="오후"){
-              Options.stoptimeEnd = 12 + Number($(this).attr('data-dur'));
-            }
-            console.log(Options.stoptimeEnd)
       });
 
       $("#ulTimes2 li a").click(function(){
-            $("#hourSelect2 button").addClass("dropdown_selected").text($(this).text()).val($(this).text());
+            $("#hourSelect2 button").addClass("dropdown_selected").text($(this).text()).val($(this).attr('data-time'));
+            var availableTime = make_available_date_format()
+            $('#id_setting_member_available_time').val(availableTime)
             check_dropdown_selected();
-            if($("#morningdaySelect2 button").val()=="오전"){
-              Options.stoptimeStart = $(this).attr('data-dur');
-            }else if($("#morningdaySelect2 button").val()=="오후"){
-              Options.stoptimeStart = 12 + Number($(this).attr('data-dur'));
-            }
-            console.log(Options.stoptimeStart)
       });
 
       $("#ulTimes3 li a").click(function(){
-            $("#timeSelect button").addClass("dropdown_selected").text($(this).text()).val($(this).text());
+            $("#timeSelect button").addClass("dropdown_selected").text($(this).text()).val($(this).attr('data-time'));
+            $('#id_setting_member_reserve_time_prohibition').val($('#timeSelect button').val())
             check_dropdown_selected();
-            Options.limit =  $(this).attr('data-dur');
       });
 
        $("#upbutton-alarm").click(function(){
