879b903be8cf553c2278f7d5ed4b0bb05649aff5
savekhg <savekhg@gmail.com>
2018-03-26 15:44:27 +0900

    강좌 개설시 center list 전달

--- pters_project/center/admin.py
+++ pters_project/center/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from center.models import CenterTb
+from center.models import CenterTb, CenterTrainerTb
 
 
 @admin.register(CenterTb)
 class CenterTbAdmin(admin.ModelAdmin):
     list_display = ('center_id', 'member_id', 'center_name', 'address', 'center_type_cd',
                     'center_img_url', 'reg_dt', 'mod_dt', 'use')
+
+
+@admin.register(CenterTrainerTb)
+class CenterTrainerTbAdmin(admin.ModelAdmin):
+    list_display = ('center_trainer_id', 'member', 'center', 'reg_dt', 'mod_dt', 'use')
+
