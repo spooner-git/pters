f55b4aff8a7f98dab99b360d4abb6dcfa83822d3
DESKTOP-Q3N0A3M\skkim <skkim.ku@gmail.com>
2018-03-26 21:49:08 +0900

    클래스 지웠을때 더이상 없으면 Add페이지로

--- pters_project/static/user/js/pters.base.set.js
+++ pters_project/static/user/js/pters.base.set.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
 
-//      var userID = "박재범"
-//      var upText = new Array("Pters",userID+" 코치님 일정")
-//
+
     var upText = "PTERS";
     var thisfilefullname = document.URL.substring(document.URL.lastIndexOf("/") + 1, document.URL.length);
 
      $("#outer_Sidenav").click(function(e){ // When any `div.container` is clicked
            closeNav(); //Sidebar가 열렸을때 회색 영역을 터치해도 Sidebar가 닫힘
       });
-//     $('#user_name').text(userID+"코치님"); //Sidebar 상단 유저 이름
-//     $('#welcome').text("안녕하세요!") //Sidebar 상단 유저 이름 아래 인사 메시지
 
 
 	 if($('meta[name="upperText"]').attr('content') == "main_trainer"){ //상단바에 텍스트 표시. 각 페이지의 Meta를 읽어와서 upperText를 셋팅
