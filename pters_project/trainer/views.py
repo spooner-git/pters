2b1c9128f7ed29f4a5694eb2d20b41b1840cd94d
savekhg <savekhg@gmail.com>
2018-03-26 19:53:29 +0900

    강좌 add/delete error message 전달

--- pters_project/trainer/views.py
+++ pters_project/trainer/views.py
@@ -2310,7 +2310,6 @@ def add_class_info_logic(request):
         except InternalError:
             error = '등록 값에 문제가 있습니다.'
 
-    print(error)
     if error is None:
         request.session['class_id'] = class_info.class_id
         log_data = LogTb(log_type='LC01', auth_member_id=request.user.id, from_member_name=request.user.last_name+request.user.first_name,
@@ -2436,4 +2435,112 @@ def delete_class_info_logic(request):
         messages.error(request, error)
 
         return redirect(next_page)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteClassInfoView(LoginRequiredMixin, AccessTestMixin, View):
+    template_name = 'trainer_error_ajax.html'
+
+    def post(self, request, *args, **kwargs):
+        class_id = request.session.get('class_id', '')
+        error = None
+
+        if class_id is None or class_id == '':
+            error = '강좌 정보를 불러오지 못했습니다.'
+
+        if error is None:
+            try:
+                class_info = ClassTb.objects.get(class_id=class_id)
+            except ObjectDoesNotExist:
+                error = '강좌 정보를 불러오지 못했습니다.'
+
+        if error is None:
+            class_info.member_view_state_cd = 'DELETE'
+            class_info.mod_dt = timezone.now()
+            class_info.save()
+
+        if error is None:
+            log_data = LogTb(log_type='LC02', auth_member_id=request.user.id, from_member_name=request.user.last_name+request.user.first_name,
+                             class_tb_id=class_id,
+                             log_info='강좌 정보', log_how='연동 해제',
+                             reg_dt=timezone.now(), ip=get_client_ip(request), use=1)
+
+            log_data.save()
+
+        if error is not None:
+            messages.error(request, error)
+
+        return render(request, self.template_name)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddClassInfoView(LoginRequiredMixin, AccessTestMixin, View):
+    template_name = 'trainer_error_ajax.html'
+
+    def post(self, request, *args, **kwargs):
+        center_id = request.POST.get('center_id', '')
+        subject_cd = request.POST.get('subject_cd', '')
+        subject_detail_nm = request.POST.get('subject_detail_nm', '')
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        class_hour = request.POST.get('class_hour', '')
+        start_hour_unit = request.POST.get('start_hour_unit', '')
+        class_member_num = request.POST.get('class_member_num', '')
+
+        error = None
+
+        if subject_cd is None or subject_cd == '':
+            error = '스케쥴 타입을 설정해주세요.'
+
+        if class_hour is None or class_hour == '':
+            class_hour = 1.0
+
+        if start_hour_unit is None or start_hour_unit == '':
+            start_hour_unit = 1.0
+
+        if start_date is None or start_date == '':
+            start_date = datetime.date.today()
+        if end_date is None or end_date == '':
+            end_date = start_date + timezone.timedelta(days=3650)
+
+        if class_member_num is None or class_member_num == '':
+            error = '수업당 최대 허용 인원을 설정해 주세요.'
+
+        if error is None:
+            try:
+                with transaction.atomic():
+
+                    class_info = ClassTb(member_id=request.user.id, center_tb_id=center_id,
+                                         subject_cd=subject_cd, start_date=start_date, end_date=end_date,
+                                         class_hour=float(class_hour), start_hour_unit=float(start_hour_unit),
+                                         member_view_state_cd='VIEW',
+                                         class_member_num=int(class_member_num), state_cd='IP',
+                                         reg_dt=timezone.now(), mod_dt=timezone.now(), use=1)
+
+                    class_info.save()
+
+            except ValueError as e:
+                error = '등록 값에 문제가 있습니다.1'
+            except IntegrityError as e:
+                error = '등록 값에 문제가 있습니다.2'
+            except TypeError as e:
+                error = '등록 값의 형태가 문제 있습니다.1'
+            except ValidationError as e:
+                error = '등록 값의 형태가 문제 있습니다.2'
+            except InternalError:
+                error = '등록 값에 문제가 있습니다.'
+
+        if error is None:
+            request.session['class_id'] = class_info.class_id
+            log_data = LogTb(log_type='LC01', auth_member_id=request.user.id,
+                             from_member_name=request.user.last_name + request.user.first_name,
+                             log_info='강좌 정보', log_how='등록',
+                             reg_dt=timezone.now(), ip=get_client_ip(request), use=1)
+
+            log_data.save()
+
+        if error is not None:
+            messages.error(request, error)
+
+        return render(request, self.template_name)
 
