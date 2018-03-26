1fd03b047f905cca4a688dac0f2ec7c87e9ec03c
savekhg <savekhg@gmail.com>
2018-03-26 19:41:29 +0900

    강좌 추가 기능

--- pters_project/trainer/admin.py
+++ pters_project/trainer/admin.py
@@ -8,7 +8,8 @@ from trainer.models import ClassTb, CompanyTb
 class ClassTbAdmin(admin.ModelAdmin):
     list_display = ('class_id', 'member_id', 'center_tb', 'subject_cd', 'subject_detail_nm',
                     'start_date', 'end_date',
-                    'class_hour', 'start_hour_unit', 'class_member_num', 'state_cd', 'schedule_check',
+                    'class_hour', 'start_hour_unit', 'class_member_num', 'state_cd',
+                    'member_view_state_cd', 'schedule_check',
                     'reg_dt', 'mod_dt', 'use')
 
 
