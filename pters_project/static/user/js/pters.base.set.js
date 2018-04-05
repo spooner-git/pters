6218a93985a02e7224cd488c1a38990dd8d330d9
DESKTOP-Q3N0A3M\skkim <skkim.ku@gmail.com>
2018-04-05 15:37:36 +0900

    로딩 이미지 -> base.js로

--- pters_project/static/user/js/pters.base.set.js
+++ pters_project/static/user/js/pters.base.set.js
@@ -8,6 +8,11 @@ $(document).ready(function(){
            closeNav(); //Sidebar가 열렸을때 회색 영역을 터치해도 Sidebar가 닫힘
       });
 
+     function sideGoPage(page){
+          $('.ajaxloadingPC').show()
+          location.href="/trainer/"+page+'/'
+      }
+
 
 	 if($('meta[name="upperText"]').attr('content') == "main_trainer"){ //상단바에 텍스트 표시. 각 페이지의 Meta를 읽어와서 upperText를 셋팅
 //	 	  $('#uptext').text(upText[0]); //Main페이지에서는 Peters 표시
