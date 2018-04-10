e5f38331e6d974296ecb269b0c7e95b01620f409
savekhg <savekhg@gmail.com>
2018-04-10 19:08:15 +0900

    pc push test

--- pters_project/static/firebase-messaging-sw.js
+++ pters_project/static/firebase-messaging-sw.js
@@ -1,28 +1,46 @@
-importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-messaging.js');
+// Import and configure the Firebase SDK
+// These scripts are made available when the app is served or deployed on Firebase Hosting
+// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+ firebase.initializeApp({
+   'messagingSenderId': '751903262384'
+ });
+var messaging = firebase.messaging();
 
-firebase.initializeApp({
-    'messagingSenderId': '751903262384' //이곳은 자신의 프로젝트 설정 => 클라우드 메세징 => 발신자ID를 기입
+/**
+ * Here is is the code snippet to initialize Firebase Messaging in the Service
+ * Worker when your app is not hosted on Firebase Hosting.
+ // [START initialize_firebase_in_sw]
+ // Give the service worker access to Firebase Messaging.
+ // Note that you can only use Firebase Messaging here, other Firebase libraries
+ // are not available in the service worker.
+ // Initialize the Firebase app in the service worker by passing in the
+ // messagingSenderId.
+ firebase.initializeApp({
+   'messagingSenderId': 'YOUR-SENDER-ID'
+ });
+ // Retrieve an instance of Firebase Messaging so that it can handle background
+ // messages.
+ const messaging = firebase.messaging();
+ // [END initialize_firebase_in_sw]
+ **/
+
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  var notificationTitle = 'Background Message Title';
+  var notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/static/common/favicon-48.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
-
-const messaging = firebase.messaging();
-
-self.addEventListener('push', function(event) {
-    const payload = event.data.json();
-    const title = payload.notification.title;
-    const options = {
-        body: payload.notification.body,
-        icon: payload.notification.icon,
-        data: payload.notification.click_action
-    };
-    console.log('pushpush test')
-    event.waitUntil(self.registration.showNotification(title, options));
-});
-
-self.addEventListener('notificationclick', function(event) {
-    console.log(event.notification);
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow(event.notification.data)
-    );
-});
+// [END background_handler]
