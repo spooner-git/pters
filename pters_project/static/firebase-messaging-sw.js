729accf442864d1ba0db3ead7deff4c9483811d7
savekhg <savekhg@gmail.com>
2018-04-10 18:58:02 +0900

    pc push test

--- pters_project/static/firebase-messaging-sw.js
+++ pters_project/static/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': '151504463082' //이곳은 자신의 프로젝트 설정 => 클라우드 메세징 => 발신자ID를 기입
+    'messagingSenderId': '751903262384' //이곳은 자신의 프로젝트 설정 => 클라우드 메세징 => 발신자ID를 기입
 });
 
 const messaging = firebase.messaging();
