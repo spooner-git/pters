68c8c2fc8e99b46dd318b40b2af3164ebb19b341
savekhg <savekhg@gmail.com>
2018-04-16 17:12:57 +0900

    수강정보 연동

--- pters_project/trainee/views.py
+++ pters_project/trainee/views.py
@@ -280,8 +280,8 @@ def get_lecture_list_by_member_id(context, member_id):
 
             for lecture_list_info in lecture_list_data:
                 try:
-                    member_lecture_data = MemberLectureTb.objects.get(member_id=member_id,
-                                                                      lecture_tb=lecture_list_info.lecture_tb_id).exclude(auth_cd='DELETE')
+                    member_lecture_data = MemberLectureTb.objects.get(~Q(auth_cd='DELETE'), member_id=member_id,
+                                                                      lecture_tb=lecture_list_info.lecture_tb_id)
                 except ObjectDoesNotExist:
                     error = '수강 정보 조회에 대한 정보가 없습니다.'
 
@@ -292,7 +292,7 @@ def get_lecture_list_by_member_id(context, member_id):
                 else:
                     error = None
 
-            if error is None:
+            if error is None and class_lecture_info.lecture_counts > 0:
                 class_lecture_info.class_type_name = pt_type_name.common_cd_nm
 
                 if class_lecture_info.class_tb.subject_detail_nm is not None and class_lecture_info.class_tb.subject_detail_nm != '':
@@ -334,8 +334,9 @@ def get_lecture_list_by_class_member_id(context, class_id, member_id):
 
         for lecture_info in lecture_list:
             try:
-                lecture_info_data = MemberLectureTb.objects.get(member_id=member_id,
-                                                                lecture_tb=lecture_info.lecture_tb_id).exclude(auth_cd='DELETE')
+                lecture_info_data = MemberLectureTb.objects.get(~Q(auth_cd='DELETE'),
+                                                                member_id=member_id,
+                                                                lecture_tb=lecture_info.lecture_tb_id)
             except ObjectDoesNotExist:
                 lecture_info_data = None
 
