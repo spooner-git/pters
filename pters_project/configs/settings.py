ef74ce74bca7fef908d17a94feaac3442ae6e3af
savekhg <savekhg@gmail.com>
2018-04-19 13:58:08 +0900

    debug

--- pters_project/configs/settings.py
+++ pters_project/configs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("PTERS_DJANGO_SECRET", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
