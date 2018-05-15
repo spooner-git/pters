e9c152602f1b0c72a0a9bbb293c09b82ca3eddbc
DESKTOP-Q3N0A3M\skkim <skkim.ku@gmail.com>
2018-05-15 10:32:24 +0900

    pt회원 본인 정보 변경

--- pters_project/static/user/js/trainee/pters.trainee.mypage.js
+++ pters_project/static/user/js/trainee/pters.trainee.mypage.js
@@ -14,14 +14,15 @@ $(document).ready(function(){
 	var currentYearhistory = [10,5,16,17,22,20,18,11,3,16,19,0] //12달 순서대로 각달의 PT 수업 갯수
 	var lastYearhistory = [1,5,10,15,29,10,12,22,27,12,18,12]
 
+
+
+	///////////////////////////그래프
 	var graphVerticalLength = 30;
 	var graphHorizontalLength = 36
 	graphSet(graphHorizontalLength,graphVerticalLength); //그래프 수직,수평 셋팅
 	dataSet(currentYearhistory); //그래프에 데이터 채우기
 	graphUnit(5); //그래프에 일정 단위마다 빨간선 긋기
 	
-
-
 	var date = new Date();
 	var year = date.getFullYear();
 	var month = date.getMonth()+1
@@ -43,7 +44,6 @@ $(document).ready(function(){
 		dataSet(currentYearhistory);
 	})
 
-
 	function graphSet(horizontal,vertical){ //그래프 눈금 셋팅 horizontal*vertical 갯수 타일
 		var loc = $('.graph_main')
 		var table = "<table>"
@@ -64,7 +64,7 @@ $(document).ready(function(){
 		loc.append(tableSet)
 	}
 
-//graphcoloring  2,5,8,11,.... 2+(3*i)
+	//graphcoloring  2,5,8,11,.... 2+(3*i)
 	function dataSet(year){
 		for(var i=0; i<12; i++){
 			var thismonth = year[i]
@@ -93,8 +93,12 @@ $(document).ready(function(){
 			$('#leftArrow').hide()
 		}
 	}
+	///////////////////////////그래프
+	fill_member_repeat_info(); // 회원 반복일정 채우기
+	fill_member_base_info(); // 회원의 기본 가입정보 채우기
 
-	 var repeat_info_dict= { 'KOR':
+	function fill_member_repeat_info(){
+		var repeat_info_dict= { 'KOR':
                                   {'DD':'매일', 'WW':'매주', '2W':'격주',
                                    'SUN':'일요일', 'MON':'월요일','TUE':'화요일','WED':'수요일','THS':'목요일','FRI':'금요일', 'SAT':'토요일'},
                                   'JAP':
@@ -105,45 +109,54 @@ $(document).ready(function(){
                                    'SUN':'Sun', 'MON':'Mon','TUE':'Tue','WED':'Wed','THS':'Thr','FRI':'Fri', 'SAT':'Sat'}
                                  }
 
-    if(ptRepeatScheduleTypeArray.length>0){
-    	for(var i=0; i<ptRepeatScheduleTypeArray.length; i++){
-    		var trainee_repeat_type = repeat_info_dict[Options.language][ptRepeatScheduleTypeArray[i]]
-	        var trainee_repeat_days = function(){
-	                                  var repeat_day_info_raw = ptRepeatScheduleWeekInfoArray[i].split('/')
-	                                  var repeat_day_info = ""
-	                                  if(repeat_day_info_raw.length>1){
-	                                      for(var j=0; j<repeat_day_info_raw.length; j++){
-	                                          var repeat_day_info = repeat_day_info + '/' + repeat_info_dict['KOR'][repeat_day_info_raw[j]].substr(0,1)
-	                                      }
-	                                  }else if(repeat_day_info_raw.length == 1){
-	                                      var repeat_day_info = repeat_info_dict['KOR'][repeat_day_info_raw[0]]
-	                                  }
-	                                  if(repeat_day_info.substr(0,1) == '/'){
-	                                      var repeat_day_info = repeat_day_info.substr(1,repeat_day_info.length)
-	                                  }
-	                                    return repeat_day_info
-	                                };
-	        var trainee_repeat_time = time_format_to_hangul(ptRepeatScheduleStartTimeArray[i])
-	        var trainee_repeat_end = '반복종료 : '+ date_format_to_hangul(ptRepeatScheduleEndDateArray[i])
-	        var trainee_repeat_day = trainee_repeat_days()
+	    if(ptRepeatScheduleTypeArray.length>0){
+	    	for(var i=0; i<ptRepeatScheduleTypeArray.length; i++){
+	    		var trainee_repeat_type = repeat_info_dict[Options.language][ptRepeatScheduleTypeArray[i]]
+		        var trainee_repeat_days = function(){
+		                                  var repeat_day_info_raw = ptRepeatScheduleWeekInfoArray[i].split('/')
+		                                  var repeat_day_info = ""
+		                                  if(repeat_day_info_raw.length>1){
+		                                      for(var j=0; j<repeat_day_info_raw.length; j++){
+		                                          var repeat_day_info = repeat_day_info + '/' + repeat_info_dict['KOR'][repeat_day_info_raw[j]].substr(0,1)
+		                                      }
+		                                  }else if(repeat_day_info_raw.length == 1){
+		                                      var repeat_day_info = repeat_info_dict['KOR'][repeat_day_info_raw[0]]
+		                                  }
+		                                  if(repeat_day_info.substr(0,1) == '/'){
+		                                      var repeat_day_info = repeat_day_info.substr(1,repeat_day_info.length)
+		                                  }
+		                                    return repeat_day_info
+		                                };
+		        var trainee_repeat_time = time_format_to_hangul(ptRepeatScheduleStartTimeArray[i])
+		        var trainee_repeat_end = '반복종료 : '+ date_format_to_hangul(ptRepeatScheduleEndDateArray[i])
+		        var trainee_repeat_day = trainee_repeat_days()
+		        $('#planBoardWrap > div.planBoard._Next_Info > p:nth-child(2)').text(trainee_next_schedule)
+			    $('._Repeat_Info p:nth-child(2)').text(trainee_repeat_type+' '
+			                                          +trainee_repeat_day + ' '
+			                                          +trainee_repeat_time)
+			    $('._Repeat_Info span').text(trainee_repeat_end)
+	    	}
+	        
+	    }else{
+	        var trainee_repeat_type = ''
+	        var trainee_repeat_day = '설정된 반복일정이 없습니다.'
+	        var trainee_repeat_time = ''
+	        var trainee_repeat_end = ''
 	        $('#planBoardWrap > div.planBoard._Next_Info > p:nth-child(2)').text(trainee_next_schedule)
 		    $('._Repeat_Info p:nth-child(2)').text(trainee_repeat_type+' '
 		                                          +trainee_repeat_day + ' '
 		                                          +trainee_repeat_time)
 		    $('._Repeat_Info span').text(trainee_repeat_end)
-    	}
-        
-    }else{
-        var trainee_repeat_type = ''
-        var trainee_repeat_day = '설정된 반복일정이 없습니다.'
-        var trainee_repeat_time = ''
-        var trainee_repeat_end = ''
-        $('#planBoardWrap > div.planBoard._Next_Info > p:nth-child(2)').text(trainee_next_schedule)
-	    $('._Repeat_Info p:nth-child(2)').text(trainee_repeat_type+' '
-	                                          +trainee_repeat_day + ' '
-	                                          +trainee_repeat_time)
-	    $('._Repeat_Info span').text(trainee_repeat_end)
-    }
+	    }
+	}
+
+	function fill_member_base_info(){
+		$('span._nameInfo').text(classTimeArray_member_name[0]) //이름
+		$('span._birthInfo').text()								//생일
+		$('span._phoneInfo').text()								//연락처
+	}
+
+	 
 
 
     $('.mode_switch_button').click(function(){
