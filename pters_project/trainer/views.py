bb08f449508e00985ba5959166a3a4c5b6d2ba16
savekhg <savekhg@gmail.com>
2018-05-18 16:08:39 +0900

    group에서 회원 제거 기능 추가중

--- pters_project/trainer/views.py
+++ pters_project/trainer/views.py
@@ -4528,8 +4528,8 @@ def delete_group_info_logic(request):
         group_info.save()
     else:
 
-        logger.error(self.request.user.last_name + ' ' + self.request.user.first_name + '[' + str(
-            self.request.user.id) + ']' + error)
+        logger.error(request.user.last_name + ' ' + request.user.first_name + '[' + str(
+            request.user.id) + ']' + error)
         messages.error(request, error)
 
     return redirect(next_page)
@@ -4574,8 +4574,8 @@ def update_group_info_logic(request):
         group_info.save()
 
     if error is not None:
-        logger.error(self.request.user.last_name + ' ' + self.request.user.first_name + '[' + str(
-            self.request.user.id) + ']' + error)
+        logger.error(request.user.last_name + ' ' + request.user.first_name + '[' + str(
+            request.user.id) + ']' + error)
         messages.error(request, error)
 
     return redirect(next_page)
@@ -4657,8 +4657,8 @@ def add_group_member_logic(request):
     next_page = request.POST.get('next_page', '/trainer/get_group_info/')
 
     if error is not None:
-        logger.error(self.request.user.last_name + ' ' + self.request.user.first_name + '[' + str(
-            self.request.user.id) + ']' + error)
+        logger.error(request.user.last_name + ' ' + request.user.first_name + '[' + str(
+            request.user.id) + ']' + error)
         messages.error(request, error)
 
     return redirect(next_page)
@@ -4855,3 +4855,78 @@ class GetGroupMemberViewAjax(LoginRequiredMixin, AccessTestMixin, ContextMixin, 
         context['member_data'] = member_data
 
         return render(request, self.template_name, context)
+
+
+@csrf_exempt
+def delete_group_member_info_logic(request):
+
+    class_id = request.session.get('class_id', '')
+    group_id = request.POST.get('group_id', '')
+    lecture_id = request.POST.get('lecture_id', '')
+    member_id = request.POST.get('member_id', '')
+    next_page = request.POST.get('next_page', '/trainer/get_group_info/')
+    error = None
+    group_lecture_info = None
+
+
+    try:
+        user_info = User.objects.get(id=member_id)
+    except ObjectDoesNotExist:
+        error = '회원 정보를 불러오지 못했습니다.'
+
+    if error is None:
+        try:
+            class_lecture_info = ClassLectureTb.objects.get(class_tb_id=class_id, lecture_tb_id=lecture_id)
+            # lecture_info = LectureTb.objects.get(lecture_id=lecture_id)
+        except ObjectDoesNotExist:
+            error = '수강정보를 불러오지 못했습니다.'
+
+    if error is None:
+        lecture_info = class_lecture_info.lecture_tb
+        schedule_data = ScheduleTb.objects.filter(lecture_tb_id=lecture_id,
+                                                  state_cd='NP')
+        schedule_data_finish = ScheduleTb.objects.filter(lecture_tb_id=lecture_id,
+                                                         state_cd='PE')
+        repeat_schedule_data = RepeatScheduleTb.objects.filter(lecture_tb_id=lecture_id)
+        # schedule_data.delete()
+        # repeat_schedule_data.delete()
+
+        member_lecture_list = MemberLectureTb.objects.filter(lecture_tb_id=lecture_info.lecture_id).exclude(
+            auth_cd='VIEW')
+        if user_info.active:
+            if len(member_lecture_list) > 0:
+                class_lecture_info.delete()
+                member_lecture_list.delete()
+                schedule_data.delete()
+                schedule_data_finish.delete()
+                repeat_schedule_data.delete()
+                lecture_info.delete()
+            else:
+                schedule_data.update(mod_dt=timezone.now(), use=0)
+                schedule_data_finish.update(mod_dt=timezone.now(), use=0)
+                class_lecture_info.auth_cd = 'DELETE'
+                # lecture_info.use = 0
+                # lecture_info.lecture_avail_count = lecture_info.lecture_rem_count
+                class_lecture_info.mod_dt = timezone.now()
+                # if lecture_info.state_cd == 'IP':
+                #    lecture_info.state_cd = 'PE'
+                class_lecture_info.save()
+                if lecture_info.state_cd == 'IP':
+                    lecture_info.state_cd = 'PE'
+                    lecture_info.mod_dt = timezone.now()
+                    lecture_info.save()
+        else:
+            class_lecture_info.delete()
+            member_lecture_list.delete()
+            schedule_data.delete()
+            schedule_data_finish.delete()
+            repeat_schedule_data.delete()
+            lecture_info.delete()
+
+    if error is not None:
+        logger.error(request.user.last_name + ' ' + request.user.first_name + '[' + str(
+            request.user.id) + ']' + error)
+        messages.error(request, error)
+
+    return redirect(next_page)
+
