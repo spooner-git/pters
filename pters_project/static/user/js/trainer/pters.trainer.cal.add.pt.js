1c90df9f6da3cb3edea851a5edb6b23113a0a79b
DESKTOP-Q3N0A3M\skkim <skkim.ku@gmail.com>
2018-05-25 19:42:23 +0900

    NTD

--- pters_project/static/user/js/trainer/pters.trainer.cal.add.pt.js
+++ pters_project/static/user/js/trainer/pters.trainer.cal.add.pt.js
@@ -746,10 +746,8 @@ $(document).ready(function(){
             var serializeArray = $form.serializeArray();
             var sendData = send_Data(serializeArray)
          }
-         console.log('addTypeSelect',addTypeSelect,serverURL)
          if(select_all_check==true){
              //ajax 회원정보 입력된 데이터 송신
-             console.log(sendData)
             if(ajax_block_during_submit == true){
                 ajax_block_during_submit = false;
                 $.ajax({
