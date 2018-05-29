b9c907373d3f30c321a1b203b83bee10f9275baf
DESKTOP-Q3N0A3M\skkim <skkim.ku@gmail.com>
2018-05-29 13:46:09 +0900

    반복일정 삭제 버그 수정

--- pters_project/static/user/js/trainer/pters.trainer.cal.week.js
+++ pters_project/static/user/js/trainer/pters.trainer.cal.week.js
@@ -602,8 +602,8 @@ $(document).ready(function(){
 			            get_repeat_info($('#cal_popup_repeatconfirm').attr('data-dbid'))
 			            set_schedule_time(jsondata)
 			            $('#members_mobile, #members_pc').html('')
-			            //get_current_member_list()
-      					//get_current_group_list()
+			            get_current_member_list()
+      					get_current_group_list()
       					get_member_lecture_list(id, 'callback', function(jsondata){
 		                    var availCount_personal = 0
 		                    for(var i= 0; i<jsondata.availCountArray.length; i++){
