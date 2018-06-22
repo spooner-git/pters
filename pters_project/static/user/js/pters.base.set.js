fe1c47d4f737981dc665439b228ced1a0ff4ff08
DESKTOP-Q3N0A3M\skkim <skkim.ku@gmail.com>
2018-06-22 14:37:05 +0900

    mshade test

--- pters_project/static/user/js/pters.base.set.js
+++ pters_project/static/user/js/pters.base.set.js
@@ -80,7 +80,7 @@ function shade_index(option){
           $('#mshade_popup').css({'z-index':$('#memberInfoPopup').css('z-index'),'display':'block'});
         }
         if($('#cal_popup_planinfo').css('display')=="block"){
-          //$('#mshade_popup').css({'display':'none'});
+          $('#mshade_popup').css({'display':'none'});
         }
         $('#mshade').css({'z-index':option,'display':'block'});
       }
