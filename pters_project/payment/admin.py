9bfcf8934c2834a8c54342f31de118ccbb220837
savekhg <savekhg@gmail.com>
2018-06-27 10:38:25 +0900

    admin page update

--- pters_project/payment/admin.py
+++ pters_project/payment/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from payment.models import PaymentInfoTb, BillingInfoTb
+
+
+@admin.register(PaymentInfoTb)
+class PaymentInfoTbAdmin(admin.ModelAdmin):
+    list_display = ('payment_info_id', 'member', 'merchandise_type_cd', 'merchant_uid', 'customer_uid',
+                    'start_date', 'end_date', 'payment_type_cd',
+                    'reg_dt', 'mod_dt', 'use')
+
+
+@admin.register(BillingInfoTb)
+class BillingInfoTbAdmin(admin.ModelAdmin):
+    list_display = ('billing_info_id', 'member', 'payment_type_cd', 'customer_uid', 'payment_date',
+                    'reg_dt', 'mod_dt', 'use')
+
