aae0d735971bcf1e9acde8dbd418c491a3fc4b0c
SK.Kim <skkim.ku@gmail.com>
2018-07-06 17:24:14 +0900

    NTD

--- pters_project/static/user/js/pters.base.set.js
+++ pters_project/static/user/js/pters.base.set.js
@@ -981,6 +981,5 @@ if(class_code.match(/BL/)){
 }
 */
 
-console.log('ymdText',class_background_img_url[2].replace(/\)/gi,""))
-$('#ymdText').css('background-image',"url('"+class_background_img_url[2].replace(/\)/gi,"")+"')")
+$('#ymdText, #upperImg').css('background-image',"url('"+class_background_img_url[2].replace(/\)/gi,"")+"')")
 
