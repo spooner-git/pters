0ecefe8d69c7c611ed11280f64653364b9eebda3
SK.Kim <skkim.ku@gmail.com>
2018-07-24 12:35:31 +0900

    통계 작업

--- pters_project/static/user/js/trainer/pters.trainer.manage.work.js
+++ pters_project/static/user/js/trainer/pters.trainer.manage.work.js
@@ -15,3 +15,18 @@ $('#selectBox .btn').click(function(){
 $('.view_duration_setter').click(function(){
     $(this).parent('div').siblings('.duration_setter_wrapper').show();
 })
+
+$('.month_sel_btn').click(function(){
+    var thisVal = Number($(this).attr('data-month'));
+    var start_date = date_format_yyyy_m_d_to_yyyy_mm_dd(substract_month(currentYear+'-'+(currentMonth+1)+'-'+1, -thisVal),'-')
+    var end_date = date_format_yyyy_m_d_to_yyyy_mm_dd(currentYear+'-'+(currentMonth+1)+'-'+1, '-')
+    console.log(start_date, end_date)
+
+    $('#call_sales_data_btn').attr({'data-startdate',start_date, 'data-enddate':end_date});
+})
+
+$('#call_sales_data_btn').click(function(){
+    var start_date = $(this).attr('data-startdate');
+    var end_date = $(this).attr('data-enddate');
+    ajax_call_sales_data(class_id, start_date, end_date);
+})
