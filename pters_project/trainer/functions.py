0886e5fd3b1661f6ae5343329e533d764520ce15
savekhg <savekhg@gmail.com>
2018-08-29 15:51:38 +0900

    [수정][강사][수강정보 연동][삭제시 로직 변경] - 회원연동o, 수강정보연동o 상태에서 수강정보 삭제시 수강정보 UN_USE 상태로 변경 안하도록

--- pters_project/trainer/functions.py
+++ pters_project/trainer/functions.py
@@ -645,7 +645,8 @@ def func_delete_lecture_info(user_id, class_id, lecture_id, member_id):
                         lecture_info.state_cd = 'PE'
                         lecture_info.lecture_avail_count = 0
                         lecture_info.lecture_rem_count = 0
-                        lecture_info.use = UN_USE
+                        # 굳이 UN_USE로 바꿀 필요 없을듯
+                        # lecture_info.use = UN_USE
                         lecture_info.save()
 
                 if len(group_data) > 0:
