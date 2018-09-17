15ffb5d69ce34aa6673f563e42710c22b55f1cf5
savekhg <savekhg@gmail.com>
2018-09-17 14:17:47 +0900

    [수정][강사][최초 로그인][프로그램 추가] - 프로그램 추가 화면 로그아웃 버튼 노출, 수업관리 미노출 - PTERS-316

--- pters_project/trainer/views.py
+++ pters_project/trainer/views.py
@@ -384,7 +384,7 @@ class AddClassView(LoginRequiredMixin, AccessTestMixin, View):
 
     def get(self, request):
         context = {}
-        cancel_redirect_url = request.GET.get('cancel_redirect_url', '/')
+        cancel_redirect_url = request.GET.get('cancel_redirect_url', '/login/logout/')
         # context = super(AddClassView, self).get_context_data(**kwargs)
         class_type_cd_data = CommonCdTb.objects.filter(common_cd='TR', use=USE).order_by('order')
         class_type_cd_data |= CommonCdTb.objects.filter(common_cd='ETC', use=USE).order_by('order')
