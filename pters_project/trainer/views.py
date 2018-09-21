90c2e6f63ae8f6f372f5a790197f908cb1a034da
savekhg <savekhg@gmail.com>
2018-09-21 11:35:44 +0900

    [수정][강사][프로그램추가][타입 추가] - PTERS-332

--- pters_project/trainer/views.py
+++ pters_project/trainer/views.py
@@ -386,8 +386,9 @@ class AddClassView(LoginRequiredMixin, AccessTestMixin, View):
         context = {}
         cancel_redirect_url = request.GET.get('cancel_redirect_url', '/login/logout/')
         # context = super(AddClassView, self).get_context_data(**kwargs)
-        class_type_cd_data = CommonCdTb.objects.filter(common_cd='TR', use=USE).order_by('order')
-        class_type_cd_data |= CommonCdTb.objects.filter(common_cd='ETC', use=USE).order_by('order')
+        class_type_cd_data = CommonCdTb.objects.filter(upper_common_cd='02', use=USE.order_by('order'))
+        # class_type_cd_data = CommonCdTb.objects.filter(common_cd='TR', use=USE).order_by('order')
+        # class_type_cd_data |= CommonCdTb.objects.filter(common_cd='ETC', use=USE).order_by('order')
 
         for class_type_cd_info in class_type_cd_data:
             class_type_cd_info.subject_type_cd = CommonCdTb.objects.filter(upper_common_cd='03',
