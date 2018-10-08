4bd19c0b20b0865ad817796bb4b8d87154e44744
SK.Kim <skkim.ku@gmail.com>
2018-10-08 16:36:15 +0900

    [수정][강사][옵션값 추가] PTERS-304 (계속 작업중)

--- pters_project/static/user/js/pters.common.option.inspector.js
+++ pters_project/static/user/js/pters.common.option.inspector.js
@@ -3,7 +3,7 @@ function pters_option_inspector(option_type, xhr, date){
     if(Options.auth.auth_option_limit == 1){
         var selected_date = date;
         var lock_date;
-        if(option_type == "plan_add" && Options.auth.auth_plan_add[0] == 1){
+        if(      option_type == "plan_create"    && Options.auth.auth_plan_create[0] == 1){
             var lock_date = Options.auth.auth_plan_add[1];
             if((compare_date2(add_date(today_YY_MM_DD, lock_date), selected_date) == false  ||  compare_date2(selected_date, substract_date(today_YY_MM_DD, -lock_date)) == false)){
                 show_caution_popup(function_lock_message(lock_date, "일정 등록"));
@@ -62,7 +62,7 @@ function pters_option_inspector(option_type, xhr, date){
                 }
                 check_dropdown_selected_addplan();
             }
-        }else if(option_type == "plan_delete" && Options.auth.auth_plan_delete[0] == 1){
+        }else if(option_type == "plan_delete"    && Options.auth.auth_plan_delete[0] == 1){
             var lock_date = Options.auth.auth_plan_delete[1];
             if((compare_date2(add_date(today_YY_MM_DD, lock_date), selected_date) == false  ||  compare_date2(selected_date, substract_date(today_YY_MM_DD, -lock_date)) == false)){
                 show_caution_popup(function_lock_message(lock_date, "일정 삭제"));
@@ -78,29 +78,69 @@ function pters_option_inspector(option_type, xhr, date){
                 enable_delete_btns_after_ajax();
                 close_info_popup('cal_popup_plandelete');
             }
-        }else if(option_type == "plan_view" && Options.auth.auth_plan_view == 1){
+        }else if(option_type == "plan_read"      && Options.auth.auth_plan_read == 1){
 
-        }else if(option_type == "member_add" && Options.auth.auth_member_add == 1){
+        }else if(option_type == "plan_update"    && Options.auth.auth_plan_update == 1){
 
-        }else if(option_type == "member_delete" && Options.auth.auth_member_delete == 1){
 
-        }else if(option_type == "member_view" && Options.auth.auth_member_view == 1){
 
-        }else if(option_type == "group_add" && Options.auth.auth_group_add == 1){
+        }else if(option_type == "member_create"  && Options.auth.auth_member_create == 1){
 
-        }else if(option_type == "group_delete" && Options.auth.auth_group_delete == 1){
+        }else if(option_type == "member_delete"  && Options.auth.auth_member_delete == 1){
 
-        }else if(option_type == "group_view" && Options.auth.auth_group_view == 1){
+        }else if(option_type == "member_read"    && Options.auth.auth_member_read == 1){
 
-        }else if(option_type == "class_add" && Options.auth.auth_class_add == 1){
+        }else if(option_type == "member_update"  && Options.auth.auth_member_update == 1){
 
-        }else if(option_type == "class_delete" && Options.auth.auth_class_delete == 1){
 
-        }else if(option_type == "class_view" && Options.auth.auth_class_view == 1){
 
-        }else if(option_type == "program_add" && Options.auth.auth_program_add == 1){
+        }else if(option_type == "group_create"   && Options.auth.auth_group_create == 1){
+
+        }else if(option_type == "group_delete"   && Options.auth.auth_group_delete == 1){
+
+        }else if(option_type == "group_read"     && Options.auth.auth_group_read == 1){
+
+        }else if(option_type == "group_update"   && Options.auth.auth_group_update == 1){
+
+
+
+        }else if(option_type == "class_create"   && Options.auth.auth_class_create == 1){
+
+        }else if(option_type == "class_delete"   && Options.auth.auth_class_delete == 1){
+
+        }else if(option_type == "class_read"     && Options.auth.auth_class_read == 1){
+
+        }else if(option_type == "class_update"   && Options.auth.auth_class_update == 1){
+
+
+
+        }else if(option_type == "package_create"   && Options.auth.auth_package_create == 1){
+
+        }else if(option_type == "package_delete"   && Options.auth.auth_package_delete == 1){
+
+        }else if(option_type == "package_read"     && Options.auth.auth_package_read == 1){
+
+        }else if(option_type == "package_update"   && Options.auth.auth_package_update == 1){
+
+
+
+        }else if(option_type == "analytics_create"   && Options.auth.auth_analytics_create == 1){
+
+        }else if(option_type == "analytics_delete"   && Options.auth.auth_analytics_delete == 1){
+
+        }else if(option_type == "analytics_read"     && Options.auth.auth_analytics_read == 1){
+
+        }else if(option_type == "analytics_update"   && Options.auth.auth_analytics_update == 1){
+
+
+
+        }else if(option_type == "program_create" && Options.auth.auth_program_create == 1){
 
         }else if(option_type == "program_delete" && Options.auth.auth_program_delete == 1){
+
+        }else if(option_type == "program_read"   && Options.auth.auth_program_read == 1){
+
+        }else if(option_type == "program_update" && Options.auth.auth_program_update == 1){
 
         }
     }
@@ -110,19 +150,19 @@ function pters_option_inspector(option_type, xhr, date){
         var type_text = option_type;
         var message;
         if(lock_date > 0){
-            message =  `<div style="margin-bottom:10px;">
-                                무료 이용자께서는 <br>
-                                <span style="font-weight:500;">${type_text}</span> 이용이 <span style="font-weight:500;">오늘 기준 ${lock_date}일로 제한</span>됩니다. <br><br>
-                                <span style="color:#fe4e65;">이용권</span> 구매로<br>
-                                <span style="color:#fe4e65;">날짜제한 없이 이용</span>해보세요!
-                            </div>`;
+            message = `<div style="margin-bottom:10px;">
+                            무료 이용자께서는 <br>
+                            <span style="font-weight:500;">${type_text}</span> 이용이 <span style="font-weight:500;">오늘 기준 ${lock_date}일로 제한</span>됩니다. <br><br>
+                            <span style="color:#fe4e65;">이용권</span> 구매로<br>
+                            <span style="color:#fe4e65;">날짜제한 없이 이용</span>해보세요!
+                        </div>`;
         }else if(lock_date == 0){
             message = `<div style="margin-bottom:10px;">
-                                        무료 이용자께서는 <br>
-                                        기능 이용이  <span style="font-weight:500;"> 제한</span>됩니다. <br><br>
-                                        <span style="color:#fe4e65;">이용권</span> 구매로<br>
-                                        <span style="color:#fe4e65;">제한 없이 이용</span>해보세요!
-                                    </div>`;
+                            무료 이용자께서는 <br>
+                            기능 이용이  <span style="font-weight:500;"> 제한</span>됩니다. <br><br>
+                            <span style="color:#fe4e65;">이용권</span> 구매로<br>
+                            <span style="color:#fe4e65;">제한 없이 이용</span>해보세요!
+                        </div>`;
         }
         return message;
     }
