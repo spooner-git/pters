213827f1c61d452ca7a09c0143d10e5d3369ff74
savekhg <savekhg@gmail.com>
2018-10-15 08:53:40 +0000

    setting true

--- pters_project/configs/settings.py
+++ pters_project/configs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("PTERS_DJANGO_SECRET", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['*']
 
