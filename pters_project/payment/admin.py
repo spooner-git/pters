4ddc2d0085ecd83271f362846866bf9938a2bc4c
savekhg <savekhg@gmail.com>
2018-10-27 23:06:18 +0900

    test

--- pters_project/payment/admin.py
+++ pters_project/payment/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import PaymentInfoTb, BillingInfoTb, ProductTb, ProductPriceTb
+from .models import PaymentInfoTb, BillingInfoTb, ProductTb, ProductPriceTb, ProductFunctionAuthTb, FunctionAuthTb
 
 
 @admin.register(PaymentInfoTb)
 class PaymentInfoTbAdmin(admin.ModelAdmin):
-    list_display = ('payment_info_id', 'name', 'member', 'merchandise_type_cd', 'merchant_uid', 'customer_uid',
-                    'start_date', 'end_date', 'payment_type_cd', 'price', 'imp_uid', 'channel', 'card_name',
+    list_display = ('payment_info_id', 'name', 'member', 'product_tb', 'merchant_uid', 'customer_uid',
+                    'start_date', 'end_date', 'paid_date', 'payment_type_cd', 'price', 'imp_uid', 'channel', 'card_name',
                     'buyer_email', 'status', 'fail_reason', 'currency', 'pay_method', 'pg_provider', 'receipt_url',
                     'buyer_name', 'reg_dt', 'mod_dt', 'use')
 
 
 @admin.register(BillingInfoTb)
 class BillingInfoTbAdmin(admin.ModelAdmin):
-    list_display = ('billing_info_id', 'member', 'pay_method', 'merchandise_type_cd',
+    list_display = ('billing_info_id', 'member', 'pay_method', 'product_tb',
                     'payment_type_cd', 'merchant_uid', 'customer_uid', 'payment_reg_date',
                     'next_payment_date', 'payed_date', 'reg_dt', 'mod_dt', 'use')
 
@@ -31,8 +31,14 @@ class ProductPriceTbAdmin(admin.ModelAdmin):
                     'payment_type_cd', 'order', 'reg_dt', 'mod_dt', 'use')
 
 
-# @admin.register(FunctionAuthTb)
-# class FunctionAuthTbAdmin(admin.ModelAdmin):
-#     list_display = ('function_auth_id', 'member', 'function_auth_type_cd', 'payment_type_cd', 'expired_date',
-#                     'reg_dt', 'mod_dt', 'use')
+@admin.register(FunctionAuthTb)
+class FunctionAuthTbAdmin(admin.ModelAdmin):
+    list_display = ('function_auth_id', 'function_auth_type_cd', 'function_auth_type_name',
+                    'reg_dt', 'mod_dt', 'use')
+
+
+@admin.register(ProductFunctionAuthTb)
+class ProductFunctionAuthTbAdmin(admin.ModelAdmin):
+    list_display = ('product_function_auth_id', 'product_tb', 'function_auth_tb', 'auth_type_cd', 'counts_type',
+                    'counts', 'reg_dt', 'mod_dt', 'use')
 
