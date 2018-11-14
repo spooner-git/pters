9322e38e25155e1eca0315c30e8361830567cb96
savekhg <savekhg@gmail.com>
2018-11-14 19:26:13 +0900

    [수정] title 변경 및 회원 생년월일 css 수정

--- pters_project/trainer/functions.py
+++ pters_project/trainer/functions.py
@@ -1006,7 +1006,7 @@ def func_add_lecture_info(user_id, user_last_name, user_first_name, class_id, pa
 
         log_data = LogTb(log_type='LB01', auth_member_id=user_id, from_member_name=user_last_name+user_first_name,
                          to_member_name=member_name, class_tb_id=class_id, lecture_tb_id=lecture_info.lecture_id,
-                         log_info='수강 정보', log_how='등록', use=USE)
+                         log_info='수강권', log_how='등록', use=USE)
 
         log_data.save()
 
