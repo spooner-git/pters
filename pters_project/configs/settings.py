a579de3ee40da4011e3ac858a4c75985b8273885
savekhg <savekhg@gmail.com>
2018-12-05 22:00:37 +0900

    [수정][설정][CACHE Test][속도 향상] -  PTERS-793

--- pters_project/configs/settings.py
+++ pters_project/configs/settings.py
@@ -65,7 +65,12 @@ MIDDLEWARE = [
     # 'django.middleware.cache.UpdateCacheMiddleware',
     # 'django.middleware.cache.FetchFromCacheMiddleware',
 ]
-
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:8000',
+    }
+}
 # CACHE_MIDDLEWARE_SECONDS = 86400
 
 INTERNAL_IPS = ('127.0.0.1', 'localhost',)
