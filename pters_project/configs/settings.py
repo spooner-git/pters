102d6c6cfe5e5691f40a1bd80ade8f4a8e73aecd
savekhg <savekhg@gmail.com>
2018-12-17 16:30:01 +0900

    [수정][강사][회원][로그아웃 불가] -PTERS-805

--- pters_project/configs/settings.py
+++ pters_project/configs/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = (
   'static/',
 )
 STATIC_ROOT = '/static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 # LOGIN URL
 LOGIN_URL = '/'
