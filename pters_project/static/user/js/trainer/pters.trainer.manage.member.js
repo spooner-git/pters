866c209c5060197a19e784dc8e225e94fbf485cc
SK.Kim <skkim.ku@gmail.com>
2019-01-21 16:41:49 +0900

    [수정][강사][일정][버그수정] PTERS-1009 -회원정보창에서 반복일정 삭제 오류

--- pters_project/static/user/js/trainer/pters.trainer.manage.member.js
+++ pters_project/static/user/js/trainer/pters.trainer.manage.member.js
@@ -962,8 +962,9 @@ $(document).ready(function(){
         var repeat_schedule_id;
         var group_id;
         if(deleteTypeSelect == "repeatinfodelete"){
-            var dbID = $(this).parent('#cal_popup_plandelete').attr('data-dbid');
-            repeat_schedule_id = $(this).parent('#cal_popup_plandelete').attr('data-repeatid');
+            var dbID = $('#cal_popup_plandelete').attr('data-dbid');
+            repeat_schedule_id = $('#cal_popup_plandelete').attr('data-repeatid');
+            console.log("dbID", dbID, "repeat_schedule_id", repeat_schedule_id)
             send_repeat_delete_personal(repeat_schedule_id, 'callback', function(jsondata){
                 get_indiv_repeat_info(dbID);
                 get_member_lecture_list(dbID);
