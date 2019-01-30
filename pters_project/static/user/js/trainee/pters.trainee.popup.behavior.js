c8500d87bb574fabd8fcf33925dd90b3ca4ec89b
savekhg <savekhg@gmail.com>
2019-01-30 21:42:43 +0900

    [수정][회원] 팝업 방식 정리 및 코드 라인 정리 - PTERS-1028

--- pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
+++ pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
@@ -1,11 +1,11 @@
 var current_activated_popup_num = 0;
-function layer_popup(option, popupname){
-    var $popup = $(`#${popupname}`);
+function layer_popup(option, popup_name){
+    var $popup = $(`#${popup_name}`);
     switch(option){
         case "open":
             $popup.css({"transform":"translateX(0)", "transition":"transform 0.3s ease-in-out", "z-index":100});
             $popup.find('.popup_mobile_content').css({"height":windowHeight - 55 + 'px'});
-            // $('#pters_mobile_menu').css({"transform":"translateX(0)", "transition":"0.5s"});
+            // $('#pters_mobile_side_menu').css({"transform":"translateX(0)", "transition":"0.5s"});
             current_activated_popup_num++;
         break;
         case "close":
