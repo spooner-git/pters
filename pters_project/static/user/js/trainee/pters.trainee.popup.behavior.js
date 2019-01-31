ebf926d29baea7f74fc5e990700151cc827c785b
SK.Kim <skkim.ku@gmail.com>
2019-01-31 16:55:48 +0900

    [수정][회원][일정페이지] 달력 리팩토링 PTERS-1031

--- pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
+++ pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
@@ -3,14 +3,14 @@ function layer_popup(option, popup_name){
     var $popup = $(`#${popup_name}`);
     switch(option){
         case "open":
-            $popup.css({"transform":"translateX(0)", "transition":"transform 0.3s ease-in-out", "z-index":100});
+            current_activated_popup_num++;
+            $popup.css({"transform":"translateX(0)", "transition":"transform 0.3s ease-in-out", "z-index":100*current_activated_popup_num});
             $popup.find('.popup_mobile_content').css({"height":windowHeight - 55 + 'px'});
             // $('#pters_mobile_side_menu').css({"transform":"translateX(0)", "transition":"0.5s"});
-            current_activated_popup_num++;
         break;
         case "close":
-            $popup.css({"transform":"translateX(100%)"});
             current_activated_popup_num--;
+            $popup.css({"transform":"translateX(100%)"});
         break;
     }
 }
