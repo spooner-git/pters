fa84a97c6ae19e0b5231bfd6559996cb5f3341ff
SK.Kim <skkim.ku@gmail.com>
2019-02-01 18:07:52 +0900

    NTD

--- pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
+++ pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
@@ -9,7 +9,6 @@ function layer_popup(option, popup_name){
             current_activated_popup_num++;
             $popup.css({"transform":"translateX(0)", "transition":"transform 0.3s ease-in-out", "z-index":100*current_activated_popup_num});
             $popup.find('.popup_mobile_content').css({"height":windowHeight - 55 + 'px'});
-            
             // $('#pters_mobile_side_menu').css({"transform":"translateX(0)", "transition":"0.5s"});
         break;
         case "close":
@@ -18,7 +17,6 @@ function layer_popup(option, popup_name){
             }
             current_activated_popup_num--;
             $popup.css({"transform":"translateX(100%)"});
-            
         break;
     }
 }
