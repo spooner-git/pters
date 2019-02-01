39547c3707019c6f461c59b9e18b3fc384bfdefd
SK.Kim <skkim.ku@gmail.com>
2019-02-01 16:44:36 +0900

    [수정][회원][일정][팝업behavior] 동작 테스트 PTERS-1031

--- pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
+++ pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
@@ -6,11 +6,15 @@ function layer_popup(option, popup_name){
             current_activated_popup_num++;
             $popup.css({"transform":"translateX(0)", "transition":"transform 0.3s ease-in-out", "z-index":100*current_activated_popup_num});
             $popup.find('.popup_mobile_content').css({"height":windowHeight - 55 + 'px'});
+
+            $('nav, .content_page').css({"transform":"translateX(-100%)", "transition":"transform 0.3s ease-in-out"});
+
             // $('#pters_mobile_side_menu').css({"transform":"translateX(0)", "transition":"0.5s"});
         break;
         case "close":
             current_activated_popup_num--;
             $popup.css({"transform":"translateX(100%)"});
+            $('nav, .content_page').css({"transform":"translateX(0%)", "transition":"transform 0.3s ease-in-out"});
         break;
     }
 }
