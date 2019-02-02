5941c337f75c54426ce1999ffdfcda7e1d6166d6
savekhg <savekhg@gmail.com>
2019-02-02 22:30:28 +0900

    [수정][회원][팝업 정리 - side menu 통일]

--- pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
+++ pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
@@ -3,7 +3,7 @@ function layer_popup(option, popup_name){
     switch(option){
         case "open":
             //첫 팝업의 경우 옆으로 밀려나도록 보이는 효과
-            if(popup_array.length == 0){
+            if(popup_array.length == 0 && popup_name != "pters_mobile_side_menu"){
                 $('nav, .content_page').css({"transform":"translateX(-100%)", "transition":"transform 0.3s ease-in-out"});
             }
             //똑같은 팝업 여러개 못뜨도록
@@ -16,7 +16,7 @@ function layer_popup(option, popup_name){
             //혹시 있을 pop 에러 방지
             if(popup_array.length > 0){
                 popup_name = popup_array.pop();
-                if(popup_array.length == 0){
+                if(popup_array.length == 0 && popup_name != "pters_mobile_side_menu"){
                     //첫 팝업의 경우 옆에서 다시 당겨오도록 보이는 효과
                     $('nav, .content_page').css({"transform":"translateX(0%)", "transition":"transform 0.3s ease-in-out"});
                 }
