c1c8f52efd8a53700ad8ef81fefed502786f2332
savekhg <savekhg@gmail.com>
2019-02-02 15:02:30 +0900

    [수정][회원][팝업 방식 정리 및 코드 라인 정리] - PTERS-1028

--- pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
+++ pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
@@ -2,23 +2,20 @@ var popup_array = [];
 function layer_popup(option, popup_name){
     switch(option){
         case "open":
-            var $popup = $(`#${popup_name}`);
             if(popup_array.length == 0){
                 $('nav, .content_page').css({"transform":"translateX(-100%)", "transition":"transform 0.3s ease-in-out"});
             }
             popup_array.push(popup_name);
-            $popup.css({"transform":"translateX(0)", "transition":"transform 0.3s ease-in-out", "z-index":100*popup_array.length});
+            $(`#${popup_name}`).css({"transform":"translateX(0)", "transition":"transform 0.3s ease-in-out", "z-index":100*popup_array.length});
             // $popup.find('.popup_mobile_content').css({"height":windowHeight - 55 + 'px'});
-            
             // $('#pters_mobile_side_menu').css({"transform":"translateX(0)", "transition":"0.5s"});
         break;
         case "close":
-            if(popup_array.length == 1){
+            popup_name = popup_array.pop();
+            if(popup_array.length == 0){
                 $('nav, .content_page').css({"transform":"translateX(0%)", "transition":"transform 0.3s ease-in-out"});
             }
-            popup_name = popup_array.pop();
-            var $popup = $(`#${popup_name}`);
-            $popup.css({"transform":"translateX(100%)"});
+            $(`#${popup_name}`).css({"transform":"translateX(100%)"});
             
         break;
     }
