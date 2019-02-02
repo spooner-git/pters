0705bfc6cff01d23ccfef477dacabe66e277aecc
SK.Kim <skkim.ku@gmail.com>
2019-02-02 12:18:37 +0900

    [수정][회원][일정][팝업] 예약완료 확인 팝업 && 팝업동작함수내 all_close 조건 추가 PTERS-1031

--- pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
+++ pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
@@ -6,7 +6,9 @@ function layer_popup(option, popup_name){
             if(popup_array.length == 0){
                 $('nav, .content_page').css({"transform":"translateX(-100%)", "transition":"transform 0.3s ease-in-out"});
             }
-            popup_array.push(popup_name);
+            if(popup_array.indexOf(popup_name) == -1){
+                popup_array.push(popup_name);
+            }
             $popup.css({"transform":"translateX(0)", "transition":"transform 0.3s ease-in-out", "z-index":100*popup_array.length});
             $popup.find('.popup_mobile_content').css({"height":windowHeight - 55 + 'px', "overflow-y":"auto"});
         break;
@@ -17,6 +19,14 @@ function layer_popup(option, popup_name){
             popup_name = popup_array.pop();
             var $popup = $(`#${popup_name}`);
             $popup.css({"transform":"translateX(100%)"});
+        break;
+        case "all_close":
+            $('nav, .content_page').css({"transform":"translateX(0%)", "transition":"transform 0.3s ease-in-out"});
+            var len = popup_array.length;
+            for(var i=0; i<len; i++){
+                $(`#${popup_array[i]}`).css({"transform":"translateX(100%)"});
+            }
+            popup_array = [];
         break;
     }
 }
