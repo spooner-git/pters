8c907729b23fb2ed9df54437345184ed000c9645
savekhg <savekhg@gmail.com>
2019-02-07 05:42:07 +0900

    [수정][회원] 캘린더 js, css 개선 - PTERS-1035

--- pters_project/static/user/js/trainee/func/pters.trainee.func.calendar.js
+++ pters_project/static/user/js/trainee/func/pters.trainee.func.calendar.js
@@ -11,12 +11,10 @@ function pters_month_calendar(calendar_name, calendar_options){
     if(calendar_options.target_html == undefined){
         calendar_options.target_html = default_targetHTML;
     }
-    if(calendar_options.design_option == undefined){
-        calendar_options.design_option = default_design_options;
+    if(calendar_options.design_options == undefined){
+        calendar_options.design_options = default_design_options;
     }
-
-    let targetHTML = calendar_options.target_html;
-    let design_options = calendar_options.design_option;
+    let design_options = calendar_options.design_options;
 
     const calendar_height = windowHeight - parseInt($('body').css('padding-top'), 10) - 20;
     const calendar_toolbox_height = 105;
@@ -28,6 +26,7 @@ function pters_month_calendar(calendar_name, calendar_options){
     let current_month = date.getMonth(); //달은 0부터 출력해줌 0~11
     let current_date = date.getDate();
     let today_yyyy_mm_dd = current_year+'-'+(current_month+1)+'-'+current_date;
+    let reference_date = today_yyyy_mm_dd;
 
     function func_month_calendar_basic_size(calendar_height){
         //달력을 감싸는 wrapper의 높이를 창크기에 맞춘다. (스크롤링 영역을 달력 안쪽으로만 잡기 위해서)
@@ -67,9 +66,10 @@ function pters_month_calendar(calendar_name, calendar_options){
     }
 
     //선택한 Index를 가지는 슬라이드에 6개행을 생성 및 날짜 채우기
-    function func_draw_month_calendar_table(reference_date, targetHTML, design_options){
+    function func_draw_month_calendar_table(input_reference_date, input_design_options){
+        let targetHTML = calendar_options.target_html;
         let $targetHTML = $(targetHTML);
-        let reference_date_split_array = reference_date.split('-');
+        let reference_date_split_array = input_reference_date.split('-');
         let reference_date_year = reference_date_split_array[0];
         let reference_date_month = reference_date_split_array[1];
         let reference_date_month_last_day = func_get_month_end_day(reference_date_year, reference_date_month);
@@ -95,14 +95,14 @@ function pters_month_calendar(calendar_name, calendar_options){
                                         </div>`;
 
         //달력의 월화수목금 표기를 만드는 부분
-        let month_day_name_text = `<div class="pters_month_cal_day_name_box obj_table_raw ${design_options["font_day_names"]}"> 
-                                    <div class="obj_table_cell_x7 ${design_options["font_color_sunday"]}">일</div>
+        let month_day_name_text = `<div class="pters_month_cal_day_name_box obj_table_raw ${input_design_options["font_day_names"]}"> 
+                                    <div class="obj_table_cell_x7 ${input_design_options["font_color_sunday"]}">일</div>
                                     <div class="obj_table_cell_x7">월</div>
                                     <div class="obj_table_cell_x7">화</div>
                                     <div class="obj_table_cell_x7">수</div>
                                     <div class="obj_table_cell_x7">목</div>
                                     <div class="obj_table_cell_x7">금</div>
-                                    <div class="obj_table_cell_x7 ${design_options["font_color_saturday"]}">토</div>  
+                                    <div class="obj_table_cell_x7 ${input_design_options["font_color_saturday"]}">토</div>  
                                    </div>`;
 
         //달력의 날짜를 만드는 부분
@@ -115,9 +115,9 @@ function pters_month_calendar(calendar_name, calendar_options){
                 let data_date = `${reference_date_year}-${reference_date_month}-${date_cache}`;
                 let font_color = "";
                 if(j == 0){
-                    font_color = design_options["font_color_sunday"];
+                    font_color = input_design_options["font_color_sunday"];
                 }else if(j == 6){
-                    font_color = design_options["font_color_saturday"];
+                    font_color = input_design_options["font_color_saturday"];
                 }
 
                 if(i==0 && j<current_month_first_date_day){ //첫번째 주일때 처리
@@ -133,14 +133,14 @@ function pters_month_calendar(calendar_name, calendar_options){
                 }
             }
 
-            let week_row = `<div class="obj_table_raw" id="week_row_${i}" style="height:${design_options["height_week_row"]}px">
+            let week_row = `<div class="obj_table_raw" id="week_row_${i}" style="height:${input_design_options["height_week_row"]}px">
                                 ${dateCellsToJoin.join('')}
                             </div>`;
             htmlToJoin.push(week_row);
 
         }
 
-        let calendar_assembled = `<div class="pters_month_cal_content_box ${design_options["font_date_basic"]}">`+htmlToJoin.join('')+'</div>';
+        let calendar_assembled = `<div class="pters_month_cal_content_box ${input_design_options["font_date_basic"]}">`+htmlToJoin.join('')+'</div>';
 
         //달력의 하단 숫자부분만 스크롤 되고, 연월일, 월화수목 표기는 스크롤 되지 않도록 사이즈를 조절한다.
         // var inner_height = calendar_height - calendar_toolbox_height - calendar_month_day_name_text_height+'px';
@@ -152,22 +152,21 @@ function pters_month_calendar(calendar_name, calendar_options){
     }
 
     function func_set_prev_next_month_button(calendar_variable){
-        let reference_date = calendar_variable.reference_date;
-        let reference_date_split_array = reference_date.split('-');
+        let input_reference_date = reference_date;
+        let reference_date_split_array = input_reference_date.split('-');
         let reference_year = reference_date_split_array[0];
         let reference_month = reference_date_split_array[1];
-        let prev_id = calendar_variable.calendar_name + "_go_prev_month";
-        let next_id = calendar_variable.calendar_name + "_go_next_month";
-
+        let inner_calendar_name = calendar_name;
+        let prev_id = inner_calendar_name + "_go_prev_month";
+        let next_id = inner_calendar_name + "_go_next_month";
+        let inner_design_options = design_options;
         $('#'+prev_id).click(function(){
             calendar_variable.draw_month_calendar_table(func_get_prev_month(reference_year, reference_month),
-                                                        calendar_variable.calendar_options.target_html,
-                                                        calendar_variable.calendar_options.design_option);
+                                                        inner_design_options);
         });
         $('#'+next_id).click(function(){
             calendar_variable.draw_month_calendar_table(func_get_next_month(reference_year, reference_month),
-                                                        calendar_variable.calendar_options.target_html,
-                                                        calendar_variable.calendar_options.design_option);
+                                                        inner_design_options);
         });
     }
 
@@ -176,10 +175,10 @@ function pters_month_calendar(calendar_name, calendar_options){
         $(`#${calendar_name}_go_${direction}_month`).trigger('click');
     }
 
-    function func_set_touch_move_to_month_calendar(){
+    function func_set_touch_move_to_month_calendar(input_target_html){
         let ts;
         let tsy;
-        let selector_body = $(targetHTML);
+        let selector_body = $(input_target_html);
         selector_body.bind("touchstart", function(e){
             ts = e.originalEvent.touches[0].clientX;
             tsy = e.originalEvent.touches[0].clientY;
@@ -210,26 +209,43 @@ function pters_month_calendar(calendar_name, calendar_options){
     }
 
     return {
-        "calendar_name": calendar_name,
+        "set_calendar_name": function(input_calendar_name){
+            //특수 문자 제외 필요
+            if(input_calendar_name!=undefined){
+                calendar_name = input_calendar_name;
+            }
+            this.draw_month_calendar_table(reference_date, design_options);
+        },
+        "get_calendar_name": function() {
+            return calendar_name;
+        },
         "init_month_calendar_table":function(){
-            func_draw_month_calendar_table(today_yyyy_mm_dd, targetHTML, design_options);
+            func_draw_month_calendar_table(today_yyyy_mm_dd, this.get_design_options());
             func_draw_month_calendar_size(calendar_height, calendar_toolbox_height, calendar_month_day_name_text_height);
             func_month_calendar_basic_size(calendar_height);
-            func_set_touch_move_to_month_calendar(targetHTML);
+            func_set_touch_move_to_month_calendar(calendar_options.target_html);
             func_set_prev_next_month_button(this);
         },
-        "calendar_options": calendar_options,
-        "reference_date": today_yyyy_mm_dd,
-        "draw_month_calendar_table":function (reference_date, targetHTML, design_options) {
-            if(reference_date==undefined){
-                reference_date = today_yyyy_mm_dd;
+        "get_design_options": function() {
+            return design_options;
+        },
+        "set_design_options": function(input_design_options){
+            if(input_design_options==undefined){
+                input_design_options = default_design_options;
             }
-            if(targetHTML==undefined){
-                targetHTML = default_targetHTML;
+            design_options = input_design_options;
+            this.draw_month_calendar_table(reference_date, input_design_options);
+        },
+        "get_reference_date": function(){
+            return reference_date;
+        },
+        "draw_month_calendar_table":function (input_reference_date, design_options) {
+            if(input_reference_date==undefined){
+                input_reference_date = today_yyyy_mm_dd;
             }
-            func_draw_month_calendar_table(reference_date, targetHTML, design_options);
+            func_draw_month_calendar_table(input_reference_date, design_options);
             func_draw_month_calendar_size(calendar_height, calendar_toolbox_height, calendar_month_day_name_text_height);
-            this.reference_date = reference_date;
+            reference_date = input_reference_date;
             func_set_prev_next_month_button(this);
         }
     };
