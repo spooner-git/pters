36eb432fa5865639036f50c1564d67a0b3110172
savekhg <savekhg@gmail.com>
2019-02-06 18:25:06 +0900

    [수정][회원][코드 정리] - PTERS-1032

--- pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
+++ pters_project/static/user/js/trainee/pters.trainee.popup.behavior.js
@@ -18,12 +18,17 @@ function func_layer_popup(option, popup_name, popup_array){
             //똑같은 팝업 여러개 못뜨도록
             if(popup_array.indexOf(popup_name) == -1){
                 popup_array.push(popup_name);
-                $(`#${popup_name}`).css({
+                $(`.${popup_name}`).parents('.popup_mobile').css({
                     "transform":"translateX(0)",
                     "transition":"transform 0.3s ease-in-out",
-                    "z-index":100*popup_array.length
+                    "z-index":100*popup_array.length,
+                    "overflow-y":"auto"
                 });
             }
+            console.log(windowHeight);
+            // if(windowHeight > ){
+
+            // }
         break;
         case "close":
             //혹시 있을 pop 에러 방지
@@ -36,14 +41,14 @@ function func_layer_popup(option, popup_name, popup_array){
                         "transition":"transform 0.3s ease-in-out"
                     });
                 }
-                $(`#${popup_name}`).css({"transform":"translateX(100%)"});
+                $(`.${popup_name}`).parents('.popup_mobile').css({"transform":"translateX(100%)"});
             }
         break;
         case "all_close":
             let popup_len = popup_array.length;
             for(let i=0; i<popup_len; i++){
                 popup_name = popup_array.pop();
-                $(`#${popup_name}`).css({"transform":"translateX(100%)"});
+                $(`.${popup_name}`).parents('.popup_mobile').css({"transform":"translateX(100%)"});
             }
             //팝업 전부 닫고 옆에서 다시 당겨오도록 보이는 효과 , side 제외
             if(popup_name != "pters_mobile_side_menu") {
