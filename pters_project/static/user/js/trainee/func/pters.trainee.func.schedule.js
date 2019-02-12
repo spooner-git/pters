3313ffccdf4b0c9eecea169dc1dea2344d544268
SK.Kim <skkim.ku@gmail.com>
2019-02-12 12:31:39 +0900

    [수정][회원][메인페이지]일정 취소하기 PTERS-1050

---
+++ pters_project/static/user/js/trainee/func/pters.trainee.func.schedule.js
@@ -0,0 +1,43 @@
+function planning_function(){
+	return  {
+				"create":function(){
+					func_create_schedule();
+				},
+				"update":function(){
+					func_update_schedule();
+				},
+				"delete" : function(){
+		    		func_cancel_schedule();
+				}
+			};
+}
+
+
+
+
+function func_cancel_schedule(){
+	// $('#form_plan_delete').submit();
+	$.ajax({
+            url: '/trainee/delete_trainee_schedule/',
+            data: $('#form_plan_delete').serialize(),
+            dataType : 'html',
+            type:'POST',
+
+            beforeSend:function(){
+
+            },
+
+            success:function(data){
+            	
+            },
+
+            complete:function(){
+            	// window.location.reload();
+            },
+
+            error:function(){
+                console.log('server error');
+            }
+        });
+
+}
