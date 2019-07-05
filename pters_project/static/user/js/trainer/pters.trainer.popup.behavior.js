febce66c42b456769fe6a38e6641d764b7921b74
SK.Kim <skkim@spooner.co.kr>
2019-07-05 15:55:58 +0900

    NTD

--- pters_project/static/user/js/trainer/pters.trainer.popup.behavior.js
+++ pters_project/static/user/js/trainer/pters.trainer.popup.behavior.js
@@ -351,6 +351,7 @@ function show_error_message(message){
 }
 
 function show_user_input_popup(prev_value, callback_after_input){
+    $(`.popup_basic_user_input input`).val('');
     layer_popup.open_layer_popup(POPUP_BASIC, 'popup_basic_user_input', POPUP_SIZE_WINDOW, POPUP_FROM_PAGE,
                                             {'popup_title':'', 'popup_comment':prev_value,
                                             'onclick_function':`${callback_after_input};layer_popup.close_layer_popup(POPUP_SIZE_WINDOW)`})
