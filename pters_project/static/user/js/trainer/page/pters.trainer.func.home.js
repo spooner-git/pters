749be7c31a596c6f1a44b6eec382633279ae6dcc
SK.Kim <skkim@spooner.co.kr>
2019-07-17 15:34:27 +0900

    코드 정리 - Dateselector, Timeselector 클래스 정리 (on page 형식으로)

--- pters_project/static/user/js/trainer/page/pters.trainer.func.home.js
+++ pters_project/static/user/js/trainer/page/pters.trainer.func.home.js
@@ -26,31 +26,12 @@ class Home{
     render_menu(){
         let html_temp = [];
 
-        // let targets = `'#temp_time_result1', '#temp_time_result2'`; 
-        let time_select_popup = `time_select_popup.open();`;
-        let date_select_popup = `date_select_popup.open();`;
-
-        let option_data = `{
-                                사과:{value: 'apple', callback: ()=>{console.log('사과 Apple')} }, 
-                                수박:{value: 'water_melon', callback: ()=>{console.log('수박 Water melon')} }, 
-                                바나나:{value: 'banana', callback: ()=>{console.log('바나나 Banana')} }
-                            }`;
-        let user_select_multi_popup = `option_select_popup.open(${option_data})`;
-
         html_temp.push(`<div>${class_center_name}</div>
                     <div>${class_type_name}</div>
                     <div>오늘 일정 ${today_schedule_num}건</div>
                     <div>총 회원수 ${total_member_num}명</div>
                     <div>이번달 신규 ${new_member_num}명</div>
                     <div>종료 임박 ${to_be_ended_member_num}명</div>
-                    <div id="temp_time_on_page"></div>
-                    <button onclick="event.preventDefault();${time_select_popup}">시간 선택 임시</button>
-                    <div id="temp_user_select_value">-</div>
-                    <button onclick="event.preventDefault();${user_select_multi_popup}">유저 선택 팝업 임시</button>
-                    <div id="temp_user_input"></div>
-                    <button onclick="show_user_input_popup ('text', 'TEST', console.log(123))">입력팝업</button>
-                    <div id="temp_date_on_page"></div>
-                    <button onclick="${date_select_popup}">날짜 선택 임시</button>
                     `);
         
         document.querySelector('#home_content_wrap').innerHTML = html_temp.join("");
--- pters_project/static/user/js/trainer/pters.trainer.component.common.js
+++ pters_project/static/user/js/trainer/pters.trainer.component.common.js
@@ -32,17 +32,21 @@ class CComponent{
         let html = `<li class="create_row" id="${id}">
                         <div class="obj_table_raw">
                             <div class="cell_title">
-                                ${icon != undefined ? `<img class="obj_icon_basic" src="${icon}">` : ""} ${title}
+                                ${icon != undefined ? `<img class="obj_icon_basic" src="${icon}">` : ""} 
+                                <span class="cell_text">${title}</span>
+                                <input type="hidden">
                             </div>
-                            <div class="cell_value">${value}</div>
                             <div class="cell_icon"><img src="/static/common/icon/navigate_next_black.png" class="obj_icon_basic"></div>
                         </div>
                     </li>`;
 
 
         $(document).off('click', `#${id}`).on('click', `#${id}`, function(e){
-            let cell_value = $(this).find('.cell_value').text();
-            onclick('clicked_crea_row_from_common_component');
+            let prev_data = $(this).find('input').val();
+            if(prev_data.length == 0){
+                prev_data = undefined;
+            }
+            onclick(prev_data);
         });
         return html;
     }
