31ec12ec8d61e47e2e69fbf010d1fd4f8873dfcf
SK.Kim <skkim@spooner.co.kr>
2019-07-18 14:29:23 +0900

    PTERS-1267 1268  - 1차 작업

--- pters_project/static/user/js/trainer/pters.trainer.component.common.js
+++ pters_project/static/user/js/trainer/pters.trainer.component.common.js
@@ -50,10 +50,86 @@ class CComponent{
             }catch{
 
             }
-    
             onclick(prev_data);
         });
         return html;
     }
+
+    static select_lecture_row (multiple_select, checked, lecture_id, lecture_name, color_code, max_member_num, onclick){
+        let html = `
+                    <li class="select_lecture_row" id="select_lecture_row_${lecture_id}">
+                        <div class="obj_table_raw">
+                            <div class="cell_lecture_color">
+                                <div style="color:${color_code}" class="lecture_color_bar">
+                                </div>
+                            </div>
+                            <div class="cell_lecture_info">
+                                <div>${lecture_name}</div>
+                                <div>정원 - ${max_member_num} 명</div>
+                            </div>
+                            <div class="cell_lecture_selected">
+                                <img src="/static/common/icon/icon_done.png" class="obj_icon_basic lecture_selected" style="display:${checked == 0 ? 'none' : 'block'}">
+                            </div>
+                        </div>
+                    </li>
+                    `;
+
+        if(multiple_select == true){
+            $(document).off('click', `#select_lecture_row_${lecture_id}`).on('click', `#select_lecture_row_${lecture_id}`, function(e){
+                if($(this).find('.lecture_selected').css('display') == 'none'){
+                    $(this).find('.lecture_selected').css('display', 'block');
+                    onclick('add');
+                }else{
+                    $(this).find('.lecture_selected').css('display', 'none');
+                    onclick('substract');
+                }
+            });
+        }else if(multiple_select == false){
+            $(document).off('click', `#select_lecture_row_${lecture_id}`).on('click', `#select_lecture_row_${lecture_id}`, function(e){
+                
+                onclick('add_single');
+                
+            });
+        }
+        return html;
+    }
+
+    static select_member_row (multiple_select, checked, member_id, member_name, member_rem_count, member_expiry, onclick){
+        let html = `
+                    <li class="select_member_row" id="select_member_row_${member_id}">
+                        <div class="obj_table_raw">
+                            <div class="cell_member_name">
+                                ${member_name}
+                            </div>
+                            <div class="cell_member_info">
+                                잔여 ${member_rem_count}회 / ${member_expiry}까지
+                            </div>
+                            <div class="cell_member_selected">
+                                <img src="/static/common/icon/icon_done.png" class="obj_icon_basic member_selected" style="display:${checked == 0 ? 'none' : 'block'}">
+                            </div>
+                        </div>
+                    </li>
+                    `;
+
+        if(multiple_select == true){
+            $(document).off('click', `#select_member_row_${member_id}`).on('click', `#select_member_row_${member_id}`, function(e){
+                if($(this).find('.member_selected').css('display') == 'none'){
+                    $(this).find('.member_selected').css('display', 'block');
+                    onclick('add');
+                }else{
+                    $(this).find('.member_selected').css('display', 'none');
+                    onclick('substract');
+                }
+            });
+        }else if(multiple_select == false){
+            $(document).off('click', `#select_member_row_${member_id}`).on('click', `#select_member_row_${member_id}`, function(e){
+                
+                onclick('add_single');
+                
+            });
+        }
+        return html;
+    }
+
 
 }
