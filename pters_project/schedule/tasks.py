b52dbf999acbde95eb0b22635f59922c7065629a
hkkim05 <hkkim@spooner.co.kr>
2019-07-24 13:03:44 +0900

    PTERS-1284 #comment celery import 문제 해결

--- pters_project/schedule/tasks.py
+++ pters_project/schedule/tasks.py
@@ -1,32 +1,35 @@
-import json
-import httplib2
+from configs.settings import DEBUG
 
-# `celery` 프로그램을 작동시키기 위한 기본 장고 세팅 값을 정한다.
-from celery import shared_task
+if DEBUG is False:
+    import json
+    import httplib2
 
-from configs import settings
+    # `celery` 프로그램을 작동시키기 위한 기본 장고 세팅 값을 정한다.
+    from celery import shared_task
+
+    from configs import settings
 
 
-@shared_task
-def task_send_fire_base(instance_id, title, message, badge_counter):
-    push_server_id = getattr(settings, "PTERS_PUSH_SERVER_KEY", '')
-    error = None
-    data = {
-        'to': instance_id,
-        'notification': {
-            'title': title,
-            'body': message,
-            'badge': badge_counter,
-            'sound': 'default'
+    @shared_task
+    def task_send_fire_base(instance_id, title, message, badge_counter):
+        push_server_id = getattr(settings, "PTERS_PUSH_SERVER_KEY", '')
+        error = None
+        data = {
+            'to': instance_id,
+            'notification': {
+                'title': title,
+                'body': message,
+                'badge': badge_counter,
+                'sound': 'default'
+            }
         }
-    }
-    body = json.dumps(data)
-    h = httplib2.Http()
+        body = json.dumps(data)
+        h = httplib2.Http()
 
-    resp, content = h.request("https://fcm.googleapis.com/fcm/send", method="POST", body=body,
-                              headers={'Content-Type': 'application/json;',
-                                       'Authorization': 'key=' + push_server_id})
-    if resp['status'] != '200':
-        error = '오류가 발생했습니다.'
+        resp, content = h.request("https://fcm.googleapis.com/fcm/send", method="POST", body=body,
+                                  headers={'Content-Type': 'application/json;',
+                                           'Authorization': 'key=' + push_server_id})
+        if resp['status'] != '200':
+            error = '오류가 발생했습니다.'
 
-    return error
+        return error
