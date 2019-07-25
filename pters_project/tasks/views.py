0bf222e574db7d40907ffbf0f0f87e113044eb27
hkkim05 <hkkim@spooner.co.kr>
2019-07-25 18:44:16 +0900

    PTERS-1249 #TEST #comment 일정 자동처리 버그 수정

--- pters_project/tasks/views.py
+++ pters_project/tasks/views.py
@@ -145,16 +145,15 @@ def update_finish_schedule_data_logic(request):
     ).filter(class_tb_id='127', state_cd=STATE_CD_NOT_PROGRESS, en_dis_type=ON_SCHEDULE_TYPE, end_dt__lte=now, use=USE
              ).annotate(schedule_auto_finish=RawSQL(query_setting_schedule_auto_finish,
                                                     [])).exclude(schedule_auto_finish=AUTO_FINISH_OFF)
-
     for not_finish_schedule_info in not_finish_schedule_data:
         member_ticket_tb_id = not_finish_schedule_info.member_ticket_tb_id
-        if not_finish_schedule_info.schedule_auto_finish == AUTO_FINISH_ON:
+        if str(not_finish_schedule_info.schedule_auto_finish) == str(AUTO_FINISH_ON):
             not_finish_schedule_info.state_cd = STATE_CD_FINISH
             not_finish_schedule_info.save()
-        elif not_finish_schedule_info.schedule_auto_finish == AUTO_ABSENCE_ON:
+        elif str(not_finish_schedule_info.schedule_auto_finish) == str(AUTO_ABSENCE_ON):
             not_finish_schedule_info.state_cd = STATE_CD_ABSENCE
             not_finish_schedule_info.save()
-        elif not_finish_schedule_info.schedule_auto_finish == AUTO_CANCEL_ON:
+        elif str(not_finish_schedule_info.schedule_auto_finish) == str(AUTO_CANCEL_ON):
             delete_schedule_info = DeleteScheduleTb(
                 schedule_id=not_finish_schedule_info.schedule_id, class_tb_id=not_finish_schedule_info.class_tb_id,
                 lecture_tb_id=not_finish_schedule_info.lecture_tb_id,
