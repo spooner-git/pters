246cf9e43d53f7532259178c75930b6c9582be40
SK.Kim <skkim@spooner.co.kr>
2019-08-05 20:43:58 +0900

    NTD - minor css

--- pters_project/static/user/js/trainer/page/pters.trainer.func.calendar.js
+++ pters_project/static/user/js/trainer/page/pters.trainer.func.calendar.js
@@ -946,7 +946,7 @@ class Calendar {
                 "week_cal_upper_box":`
                                         <div class="cal_upper_box">
                                             <button onclick="${this.instance}.move_week('prev')" style="vertical-align:middle;" hidden>이전</button>
-                                            <div style="display:inline-block;width:200px;font-size:23px;font-weight:bold;color:#3b3d3d;" onclick="${this.instance}.switch_cal_type()">
+                                            <div style="display:inline-block;width:240px;font-size:23px;font-weight:bold;color:#3b3d3d;" onclick="${this.instance}.switch_cal_type()">
                                                 <span class="display_week">${this.get_week_dates(this.current_year, this.current_month, this.current_week) ? this.get_week_dates(this.current_year, this.current_month, this.current_week).month[0] :null}월 
                                                                            ${this.get_week_dates(this.current_year, this.current_month, this.current_week) ? this.get_week_dates(this.current_year, this.current_month, this.current_week).date[0] :null}일 - 
                                                                            ${this.get_week_dates(this.current_year, this.current_month, this.current_week) ? this.get_week_dates(this.current_year, this.current_month, this.current_week).month[6]: null}월 
