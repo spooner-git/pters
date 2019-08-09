01d9cc0e6bcaa8141914f8c08d650f79c7af8a03
SK.Kim <skkim@spooner.co.kr>
2019-08-09 14:57:41 +0900

    NTD

--- pters_project/static/user/js/trainer/page/pters.trainer.func.alarm.js
+++ pters_project/static/user/js/trainer/page/pters.trainer.func.alarm.js
@@ -40,42 +40,11 @@ class Alarm {
             return false;
         }
 
-        let whole_data, length;
-
-        if(list_type == "ing"){
-            whole_data = jsondata.current_member_data;
-            length = whole_data.length;
-            this.member_ing_length = length;
-            this.member_list_type_text = "진행중";
-        }else if(list_type == "end"){
-            whole_data = jsondata.finish_member_data;
-            length = whole_data.length;
-            this.member_end_length = length;
-            this.member_list_type_text = "종료";
-        }
-
-        this.member_length = length;
 
         let html_temp = [];
         for (let i=0; i<length; i++){
-            let data = whole_data[i];
-            let member_id = data.member_id;
-            let member_name = data.member_name;
-            let member_phone = data.member_phone;
-            let member_reg = data.member_ticket_reg_count;
-            let member_rem = data.member_ticket_rem_count;
-            let end_date = data.end_date;
-            let end_date_text = DateRobot.to_text(end_date);
-            let remain_date = Math.round((new Date(end_date).getTime() - new Date().getTime()) / (1000*60*60*24));
-            let remain_alert_text = "";
-            if(remain_date < 0){
-                remain_alert_text = " <span style='color:#fe4e65;'>지남</span>";
-                remain_date = Math.abs(remain_date);
-            }
-
-            let onclick = `layer_popup.open_layer_popup(${POPUP_BASIC}, '${POPUP_ADDRESS_MEMBER_VIEW}', 100, ${POPUP_FROM_RIGHT}, {'member_id':${member_id}}, ()=>{
-                member_view_popup = new Member_view('.popup_member_view', ${member_id}, 'member_view_popup');});`;
-            let html = `<article class="member_wrapper" data-member_id="${member_id}" data-name="${member_name}" onclick="${onclick}" style="color:${list_type == "ing" ? "" : '#a3a0a0'}">
+            
+            let html = `<article class="member_wrapper" data-member_id="${member_id}" data-name="${member_name}" style="color:${list_type == "ing" ? "" : '#a3a0a0'}">
                             <div class="alarm_data_u">
                                 <img src="/static/common/icon/icon_account.png">
                             </div>                
@@ -123,7 +92,9 @@ class Alarm_func{
             success:function (data){
                 console.log(data);
                 // let jsondata = JSON.parse(data);
-                callback(data);
+                if(callback != undefined){
+                    callback(data);
+                }
             },
 
             //보내기후 팝업창 닫기
