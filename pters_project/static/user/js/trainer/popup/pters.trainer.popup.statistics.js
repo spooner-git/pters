a7d76fd6854f83cd2f8f3fc41eed2d151657262f
SK.Kim <skkim@spooner.co.kr>
2019-09-09 19:45:06 +0900

    PTERS-1402 #comment 회원 관련 통계 그래프 적용

--- pters_project/static/user/js/trainer/popup/pters.trainer.popup.statistics.js
+++ pters_project/static/user/js/trainer/popup/pters.trainer.popup.statistics.js
@@ -18,13 +18,13 @@ class Statistics{
         this.tab = "sales"; //member
 
         this.data = {
-                sales:{},
-                sales_detail:{},
-                member:{},
+                sales:[],
+                sales_detail:[],
+                member:[],
                 chart:{
                     sales:{},
                     sales_detail:{},
-                    member:{}
+                    member:{reg:[], refund:[], contract:[], lesson_complete:[]}
                 }
         };
 
@@ -35,6 +35,7 @@ class Statistics{
  
     init(){
         // this.render();
+        this.render_loading_image();
         this.set_initial_data();
     }
 
@@ -45,12 +46,16 @@ class Statistics{
                     "end_date":`2019-4-30`};
         Statistics_func.read("sales", data, (data)=>{
             this.data.sales = data;
-            this.data.chart.sales = this.data_convert_for_column_chart();
+            this.data.chart.sales = this.data_convert_for_column_chart('sales');
             this.render();
             func_set_webkit_overflow_scrolling(`${this.target.install} .wrapper_middle`);
         });
         Statistics_func.read("member", data, (data)=>{
             this.data.member = data;
+            this.data.chart.member.reg = [["유형", "대상"], ["신규 등록", data.total_month_new_reg_member], ["재등록", data.total_month_re_reg_member]];
+            this.data.chart.member.refund = [["유형", "대상"], ["전체 환불", data.total_month_all_refund_member], ["부분 환불", data.total_month_part_refund_member]];
+            this.data.chart.member.contract = this.data_convert_for_column_chart('contract');
+            this.data.chart.member.lesson_complete = this.data_convert_for_column_chart('lesson_complete');
         });
     }
 
@@ -59,7 +64,15 @@ class Statistics{
         google.charts.load('current', {'packages':['corechart']});
     
         // Set a callback to run when the Google Visualization API is loaded.
-        google.charts.setOnLoadCallback(this.draw_sales_graph);
+        if(this.tab == "sales"){
+            google.charts.setOnLoadCallback(this.draw_sales_graph);
+        }else{
+            google.charts.setOnLoadCallback(this.draw_reg_status_graph);
+            google.charts.setOnLoadCallback(this.draw_refund_status_graph);
+            google.charts.setOnLoadCallback(this.draw_contract_status_graph);
+            google.charts.setOnLoadCallback(this.draw_lesson_complete_status_graph);
+        }
+        
     }
 
     clear(){
@@ -82,6 +95,14 @@ class Statistics{
         this.load_google_chart();
     }
 
+    render_loading_image(){
+        document.querySelector(this.target.install).innerHTML = 
+            `<div style="position:fixed;top:50%;left:50%;transform:translate(-50%, -50%);text-align:center;">
+                <img src="/static/common/loading.svg">
+                <div style="font-size:12px;color:#858282">사용자 데이터를 불러오고 있습니다.</div>
+            </div>`;
+    }
+
     render_toolbox(){
         document.getElementById(this.target.toolbox).innerHTML = this.dom_assembly_toolbox();
     }
@@ -99,7 +120,7 @@ class Statistics{
         if(this.tab == "sales"){
             html = this.dom_row_sales_summary() + this.dom_row_sales_graph() + this.dom_row_sales_list();
         }else if(this.tab == "member"){
-            html = this.draw_reg_status_graph() + this.draw_refund_status_graph() + this.draw_contract_status_graph() + this.draw_lesson_complete_status_graph();
+            html = this.dom_row_member_summary() + this.dom_reg_status_graph() + this.dom_refund_status_graph() + this.dom_contract_status_graph() + this.dom_lesson_complete_status_graph();
         }
 
         return html;
@@ -111,7 +132,7 @@ class Statistics{
         let html = `
                     <div class="lecture_view_upper_box">
                         <div style="display:inline-block;width:320px;">
-                            <div style="display:inline-block;width:100px;font-size:23px;font-weight:bold;text-align:center;color:${this.tab=="sales" ? "#3d3b3b" :"#b8b4b4"}" onclick="statistics_popup.switch('member')">
+                            <div style="display:inline-block;width:100px;font-size:23px;font-weight:bold;text-align:center;color:${this.tab=="sales" ? "#3d3b3b" :"#b8b4b4"}" onclick="statistics_popup.switch('sales')">
                                 ${title}
                             </div>
                             <div style="display:inline-block;background-color:#f5f2f3;width:2px;height:16px;"></div>
@@ -149,6 +170,25 @@ class Statistics{
         return html;
     }
 
+    dom_row_member_summary(){
+        let data = this.data.member;
+
+        let length = data.month_date.length;
+        let start_month_date = data.month_date[0].replace(/-/gi, ".");
+        let end_month_date = data.month_date[length-1];
+        
+        let last_date_of_end_month = new Date(end_month_date.split('-')[0], end_month_date.split('-')[1], 0).getDate();
+        let end_month_last_date = `${end_month_date.split('-')[0]}-${end_month_date.split('-')[1]}-${last_date_of_end_month}`;
+        end_month_date = end_month_last_date.replace(/-/gi, ".");
+
+        let html = `
+                    <div style="padding:0 20px 16px 20px;border-bottom:1px solid #f5f2f3;margin-bottom:20px;">
+                        <div style="line-height:24px;font-size:15px;font-weight:500;letter-spacing:-0.7px;color:#5c5859;">${start_month_date} - ${end_month_date}</div>
+                    </div>
+                    `;
+        return html;
+    }
+
     dom_row_sales_graph(){
         let html = `<section style="width:95%;text-align:center;margin:0 auto;box-sizing:border-box">
                         <div style="font-size:15px;font-weight:500;letter-spacing:-0.7px;color:#5c5859;">월별 현황</div>
@@ -157,6 +197,43 @@ class Statistics{
                     </section>`;
         return html;
     }
+
+    dom_reg_status_graph(){
+        let html = `<section style="width:95%;text-align:left;margin:0 auto;box-sizing:border-box;border-bottom:1px solid #f5f2f3;padding:30px 0">
+                        <div style="font-size:15px;font-weight:500;letter-spacing:-0.7px;color:#5c5859;padding:0 16px;">등록 회원 현황</div>
+                        <div style="font-size:11px;font-weight:500;letter-spacing:-0.5px;color:#858282;padding:0 16px;">(단위 / 건)</div>
+                        <div id="member_reg_graph"></div>
+                    </section>`;
+        return html;
+    }
+
+    dom_refund_status_graph(){
+        let html = `<section style="width:95%;text-align:left;margin:0 auto;box-sizing:border-box;border-bottom:1px solid #f5f2f3;padding:30px 0">
+                        <div style="font-size:15px;font-weight:500;letter-spacing:-0.7px;color:#5c5859;padding:0 16px;">환불 회원 현황</div>
+                        <div style="font-size:11px;font-weight:500;letter-spacing:-0.5px;color:#858282;padding:0 16px;">(단위 / 건)</div>
+                        <div id="member_refund_graph"></div>
+                    </section>`;
+        return html;
+    }
+
+    dom_contract_status_graph(){
+        let html = `<section style="width:95%;text-align:center;margin:0 auto;box-sizing:border-box;border-bottom:1px solid #f5f2f3;padding:30px 0">
+                        <div style="font-size:15px;font-weight:500;letter-spacing:-0.7px;color:#5c5859;">월별 계약 현황</div>
+                        <div style="font-size:11px;font-weight:500;letter-spacing:-0.5px;color:#858282;">(단위 / 건)</div>
+                        <div id="contract_status_graph"></div>
+                    </section>`;
+        return html;
+    }
+
+    dom_lesson_complete_status_graph(){
+        let html = `<section style="width:95%;text-align:center;margin:0 auto;box-sizing:border-box;border-bottom:1px solid #f5f2f3;padding:30px 0">
+                        <div style="font-size:15px;font-weight:500;letter-spacing:-0.7px;color:#5c5859;">월별 레슨 완료 현황</div>
+                        <div style="font-size:11px;font-weight:500;letter-spacing:-0.5px;color:#858282;">(단위 / 건)</div>
+                        <div id="lesson_complete_status_graph"></div>
+                    </section>`;
+        return html;
+    }
+
 
     dom_row_sales_list(){
         let length = this.data.sales.month_date.length;
@@ -181,10 +258,10 @@ class Statistics{
 
     draw_sales_graph() {
         let chart_data = statistics_popup.data.chart.sales;
-        var data = new google.visualization.arrayToDataTable(chart_data);
+        let data = new google.visualization.arrayToDataTable(chart_data);
 
 
-        var options = {'title': "월별 매출 현황",
+        let options = {'title': "월별 매출 현황",
             // 'width':windowWidth,
             'height':250,
             'chartArea':{width:'75%', height:'80%'},
@@ -217,39 +294,161 @@ class Statistics{
             }
         
   
-        var chart = new google.visualization.ColumnChart(
-            document.getElementById('sales_graph'));
+            let chart = new google.visualization.ColumnChart(document.getElementById('sales_graph'));
   
         chart.draw(data, options);
     }
 
     draw_reg_status_graph(){
+        let chart_data = statistics_popup.data.chart.member.reg;
+        let data = new google.visualization.arrayToDataTable(chart_data);
 
+        // Set chart options
+        let options = {'title':"회원 등록 현황",
+                        // 'width':width,
+                        // 'height':height,
+                        colors:['#fe4e65', '#5d91f7'],
+                        titlePosition:'none',
+                        'chartArea':{width:'85%',height:'85%'},
+                        'legend':{position:'right', 'alignment':'center'},
+                        sliceVisibilityThreshold:0,
+                        pieSliceText:'value' };
+
+        let chart = new google.visualization.PieChart(document.getElementById('member_reg_graph'));
+        chart.draw(data, options);
     }
 
     draw_refund_status_graph(){
+        let chart_data = statistics_popup.data.chart.member.refund;
+        let data = new google.visualization.arrayToDataTable(chart_data);
 
+        // Set chart options
+        let options = {'title':"회원 환불 현황",
+                        // 'width':width,
+                        // 'height':height,
+                        colors:['#fe4e65', '#5d91f7'],
+                        titlePosition:'none',
+                        'chartArea':{width:'85%',height:'85%'},
+                        'legend':{position:'right', 'alignment':'center'},
+                        sliceVisibilityThreshold:0,
+                        pieSliceText:'value' };
+
+        let chart = new google.visualization.PieChart(document.getElementById('member_refund_graph'));
+        chart.draw(data, options);
     }
 
     draw_contract_status_graph(){
+        let chart_data = statistics_popup.data.chart.member.contract;
+        let data = new google.visualization.arrayToDataTable(chart_data);
 
+        let options = {'title': "월별 계약 현황",
+            // 'width':windowWidth,
+            'height':250,
+            'chartArea':{width:'75%', height:'80%'},
+             'legend':{position:'none'},
+             titlePosition:'none',
+             colors:['#fe4e65', '#5d91f7'],
+             isStacked: true,
+             hAxis: {
+                        // viewWindow: {
+                        //     min: [7, 30, 0],
+                        //     max: [19, 30, 0]
+                        // }
+                        showTextEvery:1
+                    },
+            vAxis: {
+                        // title: 'Rating (scale of 1-12)'
+                    },
+            trendlines:{0:{
+                            // type:'linear',
+                            // type:'exponential',
+                            type:'polynomial',
+                            color:'blue',
+                            lineWidth:3,
+                            opacity:0.1,
+                            showR2:true,
+                            visibleInLegend:false
+                            }
+                         }
+            }
+        
+  
+            let chart = new google.visualization.ColumnChart(document.getElementById('contract_status_graph'));
+  
+        chart.draw(data, options);
     }
 
     draw_lesson_complete_status_graph(){
+        let chart_data = statistics_popup.data.chart.member.lesson_complete;
+        let data = new google.visualization.arrayToDataTable(chart_data);
 
+        let options = {'title': "월별 레슨 완료 현황",
+            // 'width':windowWidth,
+            'height':250,
+            'chartArea':{width:'75%', height:'80%'},
+             'legend':{position:'none'},
+             titlePosition:'none',
+             colors:['#fe4e65', '#5d91f7'],
+             isStacked: true,
+             hAxis: {   
+                        // viewWindow: {
+                        //     min: [7, 30, 0],
+                        //     max: [19, 30, 0]
+                        // }
+                        showTextEvery:1
+                    },
+            vAxis: {
+                        // title: 'Rating (scale of 1-12)'
+                    },
+            trendlines:{0:{
+                            // type:'linear',
+                            // type:'exponential',
+                            type:'polynomial',
+                            color:'blue',
+                            lineWidth:3,
+                            opacity:0.1,
+                            showR2:true,
+                            visibleInLegend:false
+                            }
+                         }
+            }
+        
+  
+            let chart = new google.visualization.ColumnChart(document.getElementById('lesson_complete_status_graph'));
+  
+        chart.draw(data, options);
     }
 
-    data_convert_for_column_chart(){
-        let new_data = [['Month', 'Sales']];
-
-        let length = this.data.sales.month_date.length;
-        for(let i=0; i<length; i++){
-            new_data.push(
-                [String(Number(this.data.sales.month_date[i].split('-')[1])), (Number(this.data.sales.price[i])-Number(this.data.sales.refund_price[i]))/10000]
-            );
-        }
+    data_convert_for_column_chart(type){
+        let new_data;
+        if(type == "sales"){
+            new_data = [['X', 'Y']];
+            let length = this.data.sales.month_date.length;
+            for(let i=0; i<length; i++){
+                new_data.push(
+                    [String(Number(this.data.sales.month_date[i].split('-')[1])), (Number(this.data.sales.price[i])-Number(this.data.sales.refund_price[i]))/10000]
+                );
+            }
+        }else if(type == "contract"){
+            new_data = [['Month', 'Contract', 'Recontract']];
+            let length = this.data.member.month_date.length;
+            for(let i=0; i<length; i++){
+                new_data.push(
+                    [String(Number(this.data.member.month_date[i].split('-')[1])), Number(this.data.member.month_new_reg_member[i]), Number(this.data.member.month_re_reg_member[i])]
+                );
+            }
+        }else if(type == "lesson_complete"){
+            new_data = [['Month', 'complete']];
+            let length = this.data.member.month_date.length;
+            for(let i=0; i<length; i++){
+                new_data.push(
+                    [String(Number(this.data.member.month_date[i].split('-')[1])), Number(this.data.member.finish_schedule_count[i])]
+                );
+            }
+        }   
 
         return new_data;
+       
     }
 
     switch(tab){
