f347e0093e4de359f8a9b33e8c3e163652c46274
SK.Kim <skkim@spooner.co.kr>
2019-10-14 20:50:28 +0900

    PTERS-1447 #comment 수업 article이 display:inline-block 에서도 내부 형태(table형태)를 유지하도록

--- pters_project/static/user/js/trainer/page/pters.trainer.func.lecture.js
+++ pters_project/static/user/js/trainer/page/pters.trainer.func.lecture.js
@@ -125,19 +125,21 @@ class Lecture {
             let onclick = `layer_popup.open_layer_popup(${POPUP_BASIC}, '${POPUP_ADDRESS_LECTURE_VIEW}', 100, ${popup_style}, {'lecture_id':${lecture_id}}, ()=>{
                 lecture_view_popup = new Lecture_view('.popup_lecture_view', ${lecture_id}, 'lecture_view_popup');});`;
             let html = `<article class="lecture_wrapper" data-text="${lecture_name}" data-lectureid="${lecture_id}" onclick="${onclick}" style="color:${this.list_status_type == "ing" ? "" : '#a3a0a0'}">
-                            <div class="lecture_data_l">
-                                <div class="lecture_tag" style="background:${this.list_status_type == "ing" ? lecture_ing_bg_color : "#a3a0a0"}"></div>
-                            </div>
-                            <div class="lecture_data_c">
-                                <div class="lecture_name">
-                                    ${lecture_name} 
+                            <div>
+                                <div class="lecture_data_l">
+                                    <div class="lecture_tag" style="background:${this.list_status_type == "ing" ? lecture_ing_bg_color : "#a3a0a0"}"></div>
                                 </div>
-                                <div class="lecture_note">
-                                    정원 - ${lecture_max_member_number}명
+                                <div class="lecture_data_c">
+                                    <div class="lecture_name">
+                                        ${lecture_name} 
+                                    </div>
+                                    <div class="lecture_note">
+                                        정원 - ${lecture_max_member_number}명
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="lecture_data_r">
-                                    <div class="lecture_member_number">${this.list_status_type == "ing" ? lecture_member_number+' 명' : ""}</div>
+                                <div class="lecture_data_r">
+                                        <div class="lecture_member_number">${this.list_status_type == "ing" ? lecture_member_number+' 명' : ""}</div>
+                                </div>
                             </div>
                         </article>`;
             html_temp.push(html);
