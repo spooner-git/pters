65deef4b4db96f99193cc297288212bc13d40122
SK.Kim <skkim@spooner.co.kr>
2019-10-16 19:26:08 +0900

    PTERS-1460 #comment check_before_send 함수에서 모든 데이터 확인 // 환불후 member_ticket_modify의 상태 텍스트가 업데이트 안되는 버그 수정

--- pters_project/static/user/js/trainer/pters.trainer.const.js
+++ pters_project/static/user/js/trainer/pters.trainer.const.js
@@ -300,6 +300,7 @@ const PROGRAM_CATEGORY = {
 
 
 const TEXT = {
+    //메뉴들
     menu:{KR:"전체", EN:"Menu", JP:""},
     home:{KR:"홈", EN:"Home", JP:""},
     schedule:{KR:"일정", EN:"Schedule", JP:""},
@@ -310,13 +311,44 @@ const TEXT = {
     attend_check:{KR:"출석 체크", EN:"Attend check", JP:""},
     notification:{KR:"알림", EN:"Notification", JP:""},
     settings:{KR:"설정", EN:"Settings", JP:""},
+    service:{KR:"서비스", EN:"Service", JP:""},
     work_time:{KR:"업무 시간", EN:"Working time", JP:""},
     auto_complete:{KR:"자동 완료", EN:"Auto Complete", JP:""},
-    reserve:{KR:"예약", EN:"Reserve", JP:""},
+    member_reserve:{KR:"회원 예약", EN:"Member Reserve", JP:""},
     attend_mode:{KR:"출석 체크 모드", EN:"Attend Mode", JP:""},
     notice:{KR:"공지 사항", EN:"Notice", JP:""},
-    inquiry:{KR:"이용 문의", EN:"Q & A", JP:""},
-    help:{KR:"도움말", EN:"Help", JP:""}
+    inquiry:{KR:"이용 문의", EN:"Inquiry", JP:""},
+    help:{KR:"도움말", EN:"Help", JP:""},
+    purchase_pters_pass:{KR:"PTERS패스 구매", EN:"Buy PTERS Pass", JP:""},
+
+    //공통
+    close:{KR:"닫기", EN:"Close", JP:""},
+    done:{KR:"완료", EN:"Done", JP:""},
+    save:{KR:"저장", EN:"Save", JP:""},
+    registration:{KR:"등록", EN:"Add", JP:""},
+
+    //홈
+    change:{KR:"변경", EN:"Change", JP:""},
+    today_plans:{KR:"오늘의 일정", EN:"Today's Plan", JP:""},
+    hide_show:{KR:"접기/펼치기", EN:"Hide/Show", JP:""},
+    expiration_alert:{KR:"종료 임박 회원", EN:"Contract Expiration", JP:""},
+    sales_of_this_month:{KR:"이번달 매출", EN:"Sales of this month", JP:""},
+
+    //일정
+    monthly_calendar:{KR:"월간 달력", EN:"Monthly Calendar", JP:""},
+    weekly_calendar:{KR:"주간 달력", EN:"Weekly Calendar", JP:""},
+    repeat_schedule_list:{KR:"반복 일정 리스트", EN:"Repeat schedule list", JP:""},
+    am:{KR:"오전", EN:"AM", JP:""},
+    pm:{KR:"오후", EN:"PM", JP:""},
+
+    new_schedule:{KR:"새로운 일정", EN:"New schedule", JP:""},
+    date:{KR:"일자", EN:"Date", JP:""},
+    time:{KR:"시간", EN:"Time", JP:""},
+    repeat:{KR:"반복", EN:"Repeat", JP:""},
+    memo:{KR:"메모", EN:"Memo", JP:""},
+    
+
+
 };
 
 
@@ -341,7 +373,6 @@ const payment_agreement = {
 
         }
     },
-
     agreement:{
         KR:`
             <div>
@@ -518,7 +549,6 @@ const payment_agreement = {
                     2.회사는 회원에게 환불에 필요한 정보를 요청(회원 성명, 결제증빙서류, 전화번호, 환불 요청 계좌 등) <br/>
                     3.회원은 환불에 필요한 2.호의 정보를 회사에 제공 <br/>
                     4.회사는 이용자의 정보 제공일로부터 7일 이내 환불 처리 (회원의 명시적 의사표시가 있는 경우 차회 요금에서 상계 제공) <br/>
-
             <br/>
             </div>
             <div>
@@ -604,3 +634,5 @@ const payment_agreement = {
         JP:null
     }
 }
+
+;
