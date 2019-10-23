08732ce1e3a011f583898b875800c2a383bbcc42
SK.Kim <skkim@spooner.co.kr>
2019-10-23 11:02:40 +0900

    PTERS-1422 #comment 상단 메뉴 이름 안보이게

--- pters_project/static/user/js/trainer/popup/pters.trainer.popup.service_helper.js
+++ pters_project/static/user/js/trainer/popup/pters.trainer.popup.service_helper.js
@@ -29,7 +29,7 @@ class Service_helper {
 
     render(){
         let top_left = `<span class="icon_left"></span>`;
-        let top_center = `<span class="icon_center"><span id="ticket_name_in_popup">튜토리얼</span></span>`;
+        let top_center = `<span class="icon_center"><span id="ticket_name_in_popup" style="color:#ffffff">튜토리얼</span></span>`;
         let top_right = `<span class="icon_right"><span style="color:#fe4e65;font-weight: 500;" onclick="service_helper_popup.close();">닫기</span></span>`;
         let content =   `<section id="${this.target.toolbox}" class="obj_box_full popup_toolbox" style="border:0;">${this.dom_assembly_toolbox()}</section>
                         <section id="${this.target.content}" class="popup_content">${this.dom_assembly_content()}</section>`;
