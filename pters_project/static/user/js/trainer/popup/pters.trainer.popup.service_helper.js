5052d0765366dd1e6e39038e0724f37152add521
SK.Kim <skkim@spooner.co.kr>
2019-10-23 10:59:38 +0900

    PTERS-1422 #comment 이미지 1차 적용

--- pters_project/static/user/js/trainer/popup/pters.trainer.popup.service_helper.js
+++ pters_project/static/user/js/trainer/popup/pters.trainer.popup.service_helper.js
@@ -60,6 +60,7 @@ class Service_helper {
         let content_3 = this.dom_content_3();
         let content_4 = this.dom_content_4();
         let content_5 = this.dom_content_5();
+        let content_6 = this.dom_content_6();
         let html = `<div class="swiper-container" id="service_helper_content_slide">
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">${content_1}</div>
@@ -67,46 +68,86 @@ class Service_helper {
                             <div class="swiper-slide">${content_3}</div>
                             <div class="swiper-slide">${content_4}</div>
                             <div class="swiper-slide">${content_5}</div>
+                            <div class="swiper-slide">${content_6}</div>
                         </div>
-                        <div class="swiper-pagination"></div>
-                        <div class="swiper-button-next"></div>
-                        <div class="swiper-button-prev"></div>
-                    </div>`;
+                        
+                    </div>
+                    <div class="swiper-pagination"></div>`;
 
         return html;
     }
 
     dom_content_1(){
         let html = `<div class="content_image_wrapper">
-                        <img src="/static/user/res/demo/1.jpg">
+                        <div class="content_text_left_top">
+                            <div style="font-size:22px;font-weight:500;letter-spacing:-1.2px;">강사를 위한</div>
+                            <div style="font-size:22px;font-weight:bold;letter-spacing:-1.2px;">운영/관리 서비스</div>
+                            <div style="font-size:22px;font-weight:bold;color:#fe4e65;letter-spacing:-1.2px;">피터스</div>
+                        </div>
+                        <div class="content_text_left_bottom">
+                            <div style="font-size:11px;">오른쪽으로 넘겨 사용법을 확인 해보세요.</div>
+                        </div>
+                        <img src="/static/common/img/tutorial/pters_tutorial_1.png">
                     </div>`;
         return html;
     }
 
     dom_content_2(){
         let html = `<div class="content_image_wrapper">
-                        <img src="/static/user/res/demo/2.jpg">
+                        <div class="content_text_left_top">
+                            <div style="font-size:13px;font-weight:500;letter-spacing:-1.2px;color:#fe4e65;margin-bottom:10px;">Step 1. 프로그램</div>
+                            <div style="font-size:16px;font-weight:500;letter-spacing:-1.2px;">자신에게 해당되는</div>
+                            <div style="font-size:16px;font-weight:bold;letter-spacing:-1.2px;">프로그램 선택</div>
+                        </div>
+                        <img src="/static/common/img/tutorial/pters_tutorial_2.png">
                     </div>`;
         return html;
     }
 
     dom_content_3(){
         let html = `<div class="content_image_wrapper">
-                        <img src="/static/user/res/demo/3.jpg">
+                        <div class="content_text_left_top">
+                            <div style="font-size:13px;font-weight:500;letter-spacing:-1.2px;color:#fe4e65;margin-bottom:10px;">Step 2. 수업</div>
+                            <div style="font-size:16px;font-weight:500;letter-spacing:-1.2px;">자신이 진행하게 될</div>
+                            <div style="font-size:16px;font-weight:bold;letter-spacing:-1.2px;">수업의 이름, 유형, 진행 시간 등 설정</div>
+                        </div>
+                        <img src="/static/common/img/tutorial/pters_tutorial_3.png">
                     </div>`;
         return html;
     }
 
     dom_content_4(){
         let html = `<div class="content_image_wrapper">
-                        <img src="/static/user/res/demo/4.jpg">
+                        <div class="content_text_left_top">
+                            <div style="font-size:13px;font-weight:500;letter-spacing:-1.2px;color:#fe4e65;margin-bottom:10px;">Step 3. 수강권</div>
+                            <div style="font-size:16px;font-weight:500;letter-spacing:-1.2px;">내가 만든 수업들로</div>
+                            <div style="font-size:16px;font-weight:bold;letter-spacing:-1.2px;">다양한 수강권을 생성</div>
+                        </div>
+                        <img src="/static/common/img/tutorial/pters_tutorial_4.png">
                     </div>`;
         return html;
     }
 
     dom_content_5(){
         let html = `<div class="content_image_wrapper">
-                        <img src="/static/user/res/demo/5.jpg">
+                        <div class="content_text_left_top">
+                            <div style="font-size:13px;font-weight:500;letter-spacing:-1.2px;color:#fe4e65;margin-bottom:10px;">Step 4. 회원</div>
+                            <div style="font-size:16px;font-weight:500;letter-spacing:-1.2px;">회원 정보를 기입하고</div>
+                            <div style="font-size:16px;font-weight:bold;letter-spacing:-1.2px;">등록할 수강권을 선택</div>
+                        </div>
+                        <img src="/static/common/img/tutorial/pters_tutorial_5.png">
+                    </div>`;
+        return html;
+    }
+
+    dom_content_6(){
+        let html = `<div class="content_image_wrapper">
+                        <div class="content_text_left_top">
+                            <div style="font-size:13px;font-weight:500;letter-spacing:-1.2px;color:#fe4e65;margin-bottom:10px;">Step 5. 일정</div>
+                            <div style="font-size:16px;font-weight:500;letter-spacing:-1.2px;">등록한 수업과 회원을 가지고</div>
+                            <div style="font-size:16px;font-weight:bold;letter-spacing:-1.2px;">수업 일정을 등록</div>
+                        </div>
+                        <img src="/static/common/img/tutorial/pters_tutorial_6.png">
                     </div>`;
         return html;
     }
@@ -132,17 +173,17 @@ class Service_helper {
                 el: '.swiper-pagination',
                 type: 'bullets',
             },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            }
+            // navigation: {
+            //     nextEl: '.swiper-button-next',
+            //     prevEl: '.swiper-button-prev',
+            // }
         });
     }
 
     close(){
         let message = `<span style="display:block;font-weight:bold;font-size:15px;margin:0;margin-bottom:8px;">튜토리얼을 종료 하시겠습니까?</span>
                         언제든 다시 볼 수 있습니다.
-                       <span style="display:block;color:#fe4e65;margin:0;">( 전체 메뉴 > 튜토리얼 )</span>`;
+                       <span style="display:block;color:#fe4e65;margin:0;">( 전체 메뉴 내 도움말 )</span>`;
         show_user_confirm (message, ()=>{
             layer_popup.close_layer_popup(); //컨펌 팝업 닫기
             layer_popup.close_layer_popup(); //튜토리얼 닫기
