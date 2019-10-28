44084ce511f23cc3a8556d653d7f4a310e34a63d
hkkim05 <hkkim@spooner.co.kr>
2019-10-28 14:42:59 +0900

    PTERS-1588 #TEST #comment 엔터, 띄어쓰기 입력 일괄 적용

--- pters_project/static/user/js/trainer/popup/pters.trainer.popup.member_view.js
+++ pters_project/static/user/js/trainer/popup/pters.trainer.popup.member_view.js
@@ -523,7 +523,7 @@ class Member_view{
         let icon_r_text = "";
         let style = null;
         let disabled = false;
-        let pattern = "[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\-_+.,@ 一-龠々ぁ-んーァ-ヾ\u318D\u119E\u11A2\u2022\u2025a\u00B7\uFE55]{0,255}";
+        let pattern = "[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\-_+.,@\\s 一-龠々ぁ-んーァ-ヾ\u318D\u119E\u11A2\u2022\u2025a\u00B7\uFE55]{0,255}";
         let pattern_message = "+ - _ @ . , 제외 특수문자는 입력 불가";
         let required = "";
         let html = CComponent.create_input_row (id, title, placeholder, icon, icon_r_visible, icon_r_text, style, disabled, (input_data)=>{
@@ -1046,7 +1046,7 @@ class Member_simple_view{
         let icon_r_text = "";
         let style = {"flex":"1 1 0", "padding":"8px 0", "font-size":"13px"};
         let disabled = true;
-        let pattern = "[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\-_+.,@ 一-龠々ぁ-んーァ-ヾ\u318D\u119E\u11A2\u2022\u2025a\u00B7\uFE55]{0,255}";
+        let pattern = "[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\-_+.,@\\s 一-龠々ぁ-んーァ-ヾ\u318D\u119E\u11A2\u2022\u2025a\u00B7\uFE55]{0,255}";
         let pattern_message = "+ - _ @ . , 제외 특수문자는 입력 불가";
         let required = "";
         let html_data = CComponent.create_input_row(id, title, placeholder, icon, icon_r_visible, icon_r_text, style, disabled, (input_data)=>{
