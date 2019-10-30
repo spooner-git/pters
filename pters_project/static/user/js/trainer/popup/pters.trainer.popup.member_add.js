31e3f7decda19c68f7e392d01e4f9a43512d4fc6
SK.Kim <skkim@spooner.co.kr>
2019-10-30 16:39:30 +0900

    PTERS-1595 #comment 버튼 사이 divider 추가

--- pters_project/static/user/js/trainer/popup/pters.trainer.popup.member_add.js
+++ pters_project/static/user/js/trainer/popup/pters.trainer.popup.member_add.js
@@ -573,12 +573,13 @@ class Member_add{
         });
         
         let wrapper_style = "display:flex;padding:0px 0 0px 20px;font-size:12px;";
+        let divider_style = "flex-basis:1px;height:20px;margin-top:10px;background-color:#f5f2f2;";
         let html = `<div style="${wrapper_style}">
-                        ${button_week_2} <div style="flex-basis:1px;height:100%;background-color:#f5f2f2;"></div>
-                        ${button_month_1} <div style="flex-basis:1px;height:100%;background-color:#f5f2f2;"></div>
-                        ${button_year_1} <div style="flex-basis:1px;height:100%;background-color:#f5f2f2;"></div>
-                        ${button_no_duration} <div style="flex-basis:1px;height:100%;background-color:#f5f2f2;"></div>
-                        ${button_clear} <div style="flex-basis:1px;height:100%;background-color:#f5f2f2;"></div>
+                        ${button_week_2} <div style="${divider_style}"></div>
+                        ${button_month_1} <div style="${divider_style}"></div>
+                        ${button_year_1} <div style="${divider_style}"></div>
+                        ${button_no_duration} <div style="${divider_style}"></div>
+                        ${button_clear}
                     </div>`;
 
         return html;
@@ -641,7 +642,7 @@ class Member_add{
         let button_delete = CComponent.button ("button_delete", "지우기", button_style, ()=>{ this.data.ticket_price = 0;this.render_content(); });
         
         let wrapper_style = "display:flex;padding:0px 0 0px 20px;font-size:12px;";
-        let divider_style = "flex-basis:1px;height:100%;background-color:#f5f2f2;";
+        let divider_style = "flex-basis:1px;height:20px;margin-top:10px;background-color:#f5f2f2;";
         let html = `<div style="${wrapper_style}">
                         ${button_100} <div style="${divider_style}"></div>
                         ${button_50} <div style="${divider_style}"></div>
