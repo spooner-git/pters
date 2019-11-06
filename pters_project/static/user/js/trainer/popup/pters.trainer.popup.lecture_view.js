24919f27cddf8a53f7304723820aff3c0bac2dc6
hkkim05 <hkkim@spooner.co.kr>
2019-11-06 18:23:43 +0900

    PTERS-1633 #TEST #comment 수업 정원/색상 바뀌는 경우 일정 처리

--- pters_project/static/user/js/trainer/popup/pters.trainer.popup.lecture_view.js
+++ pters_project/static/user/js/trainer/popup/pters.trainer.popup.lecture_view.js
@@ -289,7 +289,7 @@ class Lecture_view{
 
     dom_row_option_select_capacity(){
         let user_option = {
-            stay:{text:"이전 일정은 변경하지 않음", callback:()=>{this.update_this_to_all_plans = OFF;layer_popup.close_layer_popup();this.send_data();layer_popup.enable_shade_click_close(); }},
+            stay:{text:"지난 일정은 변경하지 않음", callback:()=>{this.update_this_to_all_plans = OFF;layer_popup.close_layer_popup();this.send_data();layer_popup.enable_shade_click_close(); }},
             change:{text:"모두 변경", callback:()=>{this.update_this_to_all_plans = ON;layer_popup.close_layer_popup();this.send_data();layer_popup.enable_shade_click_close(); }}
         };
         let options_padding_top_bottom = 16;
