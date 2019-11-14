bebaf5477c7e96ea1cf836d1dd2ca391f951fbcf
SK.Kim <skkim@spooner.co.kr>
2019-11-14 16:21:37 +0900

    NTD

--- pters_project/static/user/js/trainer/pters.trainer.component.common.js
+++ pters_project/static/user/js/trainer/pters.trainer.component.common.js
@@ -708,6 +708,38 @@ class CImg{
                     `;
         return svg;
     }
+
+    static home(ex_class){
+        let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="22" viewBox="0 0 20 22" class="${ex_class}">
+                        <path fill="#5C5859" fill-rule="evenodd" d="M2.5 19.5h15v-15h-15v15zm17-17v17.003c0 1.103-.89 1.997-1.997 1.997H2.497A1.995 1.995 0 0 1 .5 19.503V2.5h4v-2h2v2h7v-2h2v2h4z"/>
+                    </svg>
+                    `;
+        return svg;
+    }
+
+    static home(ex_class){
+        let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="22" viewBox="0 0 20 22" class="${ex_class}">
+                        <path fill="#5C5859" fill-rule="evenodd" d="M2.5 19.5h15v-15h-15v15zm17-17v17.003c0 1.103-.89 1.997-1.997 1.997H2.497A1.995 1.995 0 0 1 .5 19.503V2.5h4v-2h2v2h7v-2h2v2h4z"/>
+                    </svg>
+                    `;
+        return svg;
+    }
+
+    static home(ex_class){
+        let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="22" viewBox="0 0 20 22" class="${ex_class}">
+                        <path fill="#5C5859" fill-rule="evenodd" d="M2.5 19.5h15v-15h-15v15zm17-17v17.003c0 1.103-.89 1.997-1.997 1.997H2.497A1.995 1.995 0 0 1 .5 19.503V2.5h4v-2h2v2h7v-2h2v2h4z"/>
+                    </svg>
+                    `;
+        return svg;
+    }
+
+    static home(ex_class){
+        let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="22" viewBox="0 0 20 22" class="${ex_class}">
+                        <path fill="#5C5859" fill-rule="evenodd" d="M2.5 19.5h15v-15h-15v15zm17-17v17.003c0 1.103-.89 1.997-1.997 1.997H2.497A1.995 1.995 0 0 1 .5 19.503V2.5h4v-2h2v2h7v-2h2v2h4z"/>
+                    </svg>
+                    `;
+        return svg;
+    }
 }
 
 
