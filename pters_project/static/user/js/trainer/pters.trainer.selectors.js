2dfaf52efa43a23240b8975433c9ff98fde66453
SK.Kim <skkim@spooner.co.kr>
2019-12-05 14:55:39 +0900

    PTERS-1745 #comment 일지 이미지 업로드 제한 // 사이즈 800px (가로 세로), 최대 2장 첨부

--- pters_project/static/user/js/trainer/pters.trainer.selectors.js
+++ pters_project/static/user/js/trainer/pters.trainer.selectors.js
@@ -5135,48 +5135,122 @@ class BoardWriter_for_daily_record{
         let date = new Date();
         let content_img_file_name = file.name+'_'+file.lastModified+'_'+date.getTime();
         let form_data = new FormData();
-        form_data.append('content_img_file', file);
-        form_data.append('content_img_file_name', file.lastModified+'_'+file.name);
-        form_data.append('schedule_id', this.data.schedule_id);
-        // form_data.append('board_type_cd', this.data.category_selected['type'].value);
-        let self = this;
-        $.ajax({
-            url: '/schedule/update_daily_record_content_img/',
-            data: form_data,
-            dataType : 'JSON',
-            type:'POST',
-            processData: false,
-            contentType: false,
-            enctype:'multipart/form-data',
+        this.resizeImage(file, (resized_image_blob)=>{
+            form_data.append('content_img_file', resized_image_blob );
+            form_data.append('content_img_file_name', file.lastModified+'_'+file.name);
+            form_data.append('schedule_id', this.data.schedule_id);
+            // form_data.append('board_type_cd', this.data.category_selected['type'].value);
+            let self = this;
+            $.ajax({
+                url: '/schedule/update_daily_record_content_img/',
+                data: form_data,
+                dataType : 'JSON',
+                type:'POST',
+                processData: false,
+                contentType: false,
+                enctype:'multipart/form-data',
 
-            beforeSend: function (xhr, settings) {
-                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                beforeSend: function (xhr, settings) {
+                    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                    }
+                    ajax_load_image(SHOW);
+                },
+
+                success:function(data){
+                    check_app_version(data.app_version);
+                    if(data.messageArray != undefined) {
+                        if (data.messageArray.length > 0) {
+                            show_error_message(data.messageArray);
+                            return false;
+                        }
+                    }
+                    $('#board_writer_content_input').summernote('insertImage', data.img_url);
+                    self.data.images[`${data.img_url}`] = null;
+                },
+
+                complete:function(){
+                    ajax_load_image(HIDE);
+                },
+
+                error:function(){
+                    //alert('통신이 불안정합니다.');
+                    show_error_message('통신이 불안정합니다.');
                 }
-                ajax_load_image(SHOW);
-            },
+            });
+        });
+    }
 
-            success:function(data){
-                check_app_version(data.app_version);
-                if(data.messageArray != undefined) {
-                    if (data.messageArray.length > 0) {
-                        show_error_message(data.messageArray);
-                        return false;
+    resizeImage(file, callback) {
+        // 문서내에 img 객체를 생성합니다
+        var img = document.createElement("img");
+        // 파일을 읽을 수 있는 File Reader 를 생성합니다
+        var reader = new FileReader();
+
+        let self = this;
+        reader.onload = function(event){
+            let file_DataURL = event.target.result;
+            img.src = file_DataURL;
+            // HTML5 canvas 객체를 생성합니다
+            img.onload = ()=>{
+                var canvas = document.createElement("canvas");      
+                var ctx = canvas.getContext("2d");
+
+                // 캔버스에 업로드된 이미지를 그려줍니다
+                ctx.drawImage(img, 0, 0);
+
+                // 최대폭을 400 으로 정했다고 가정했을때
+                // 최대폭을 넘어가는 경우 canvas 크기를 변경해 줍니다.
+                var MAX_WIDTH = 800;
+                var MAX_HEIGHT = 800;
+                var width = img.width;
+                var height = img.height;
+
+                if (width > height) {
+                    if (width > MAX_WIDTH) {
+                        height *= MAX_WIDTH / width;
+                        width = MAX_WIDTH;
+                    }
+                } else {
+                    if (height > MAX_HEIGHT) {
+                        width *= MAX_HEIGHT / height;
+                        height = MAX_HEIGHT;
                     }
                 }
-                $('#board_writer_content_input').summernote('insertImage', data.img_url);
-                self.data.images[`${data.img_url}`] = null;
-            },
+                canvas.width = width;
+                canvas.height = height;
 
-            complete:function(){
-                ajax_load_image(HIDE);
-            },
-
-            error:function(){
-                //alert('통신이 불안정합니다.');
-                show_error_message('통신이 불안정합니다.');
+                // canvas에 변경된 크기의 이미지를 다시 그려줍니다.
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0, width, height);
+            
+                // canvas 에 있는 이미지를 img 태그로 넣어줍니다
+                var dataurl = canvas.toDataURL("image/png");
+                callback(self.dataURItoBlob(dataurl));
             }
-        });
+        };
+        reader.readAsDataURL(file);
+    }
+        
+
+    dataURItoBlob(dataURI) {
+        // convert base64/URLEncoded data component to raw binary data held in a string
+        var byteString;
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+            byteString = atob(dataURI.split(',')[1]);
+        else
+            byteString = unescape(dataURI.split(',')[1]);
+    
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    
+        // write the bytes of the string to a typed array
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+        
+        return new Blob([ia], {type:mimeString});
     }
 
     request_list (callback){
@@ -5210,6 +5284,13 @@ class BoardWriter_for_daily_record{
             }
             if(selected_value_ok == false){
                 show_error_message("카테고리를 선택해주세요.");
+                return false;
+            }
+        }
+
+        if(this.data.content.match(/<img/g) != null){
+            if(this.data.content.match(/img/g).length > 2){
+                show_error_message("이미지는 2장만 첨부 가능합니다.");
                 return false;
             }
         }
