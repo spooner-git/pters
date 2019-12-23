4698fedc667659bc993081c2d927d4c983a0d790
SK.Kim <skkim@spooner.co.kr>
2019-12-23 11:09:48 +0900

    PTERS-1805 #comment 준비

---
+++ pters_project/static/user/js/trainer/pters.trainer.text.js
@@ -0,0 +1,52 @@
+const TEXT = {
+    //메뉴들
+    menu:{KR:"전체", EN:"Menu", JP:""},
+    home:{KR:"홈", EN:"Home", JP:""},
+    schedule:{KR:"일정", EN:"Schedule", JP:""},
+    member:{KR:"회원", EN:"Member", JP:""},
+    lecture:{KR:"수업", EN:"Lecture", JP:""},
+    ticket:{KR:"수강권", EN:"Ticket", JP:""},
+    statistics:{KR:"통계", EN:"Statistics", JP:""},
+    attend_check:{KR:"출석 체크", EN:"Attend check", JP:""},
+    notification:{KR:"알림", EN:"Notification", JP:""},
+    settings:{KR:"설정", EN:"Settings", JP:""},
+    service:{KR:"서비스", EN:"Service", JP:""},
+    work_time:{KR:"업무 시간", EN:"Working time", JP:""},
+    auto_complete:{KR:"자동 완료", EN:"Auto Complete", JP:""},
+    member_reserve:{KR:"회원 예약", EN:"Member Reserve", JP:""},
+    attend_mode:{KR:"출석 체크 모드", EN:"Attend Mode", JP:""},
+    notice:{KR:"공지 사항", EN:"Notice", JP:""},
+    inquiry:{KR:"이용 문의", EN:"Inquiry", JP:""},
+    help:{KR:"도움말", EN:"Help", JP:""},
+    purchase_pters_pass:{KR:"PTERS패스 구매", EN:"Buy PTERS Pass", JP:""},
+
+    //공통
+    close:{KR:"닫기", EN:"Close", JP:""},
+    done:{KR:"완료", EN:"Done", JP:""},
+    save:{KR:"저장", EN:"Save", JP:""},
+    registration:{KR:"등록", EN:"Add", JP:""},
+
+    //홈
+    change:{KR:"변경", EN:"Change", JP:""},
+    today_plans:{KR:"오늘의 일정", EN:"Today's Plan", JP:""},
+    today_no_plans:{KR:"오늘의 일정이 없습니다.", EN:"There's no plan today", JP:"今日の日程がありません。"},
+    hide_show:{KR:"접기/펼치기", EN:"Hide/Show", JP:""},
+    expiration_alert:{KR:"종료 임박 회원", EN:"Contract Expiration", JP:""},
+    sales_of_this_month:{KR:"이번달 매출", EN:"Sales of this month", JP:""},
+
+    //일정
+    monthly_calendar:{KR:"월간 달력", EN:"Monthly Calendar", JP:""},
+    weekly_calendar:{KR:"주간 달력", EN:"Weekly Calendar", JP:""},
+    repeat_schedule_list:{KR:"반복 일정 리스트", EN:"Repeat schedule list", JP:""},
+    am:{KR:"오전", EN:"AM", JP:""},
+    pm:{KR:"오후", EN:"PM", JP:""},
+
+    new_schedule:{KR:"새로운 일정", EN:"New schedule", JP:""},
+    date:{KR:"일자", EN:"Date", JP:""},
+    time:{KR:"시간", EN:"Time", JP:""},
+    repeat:{KR:"반복", EN:"Repeat", JP:""},
+    memo:{KR:"메모", EN:"Memo", JP:""},
+    
+
+
+};
