e32a0fa8b0f813a206de33c4f7a62092ad9c7577
SK.Kim <skkim@spooner.co.kr>
2020-02-18 20:02:42 +0900

    PTERS-1995 #TEST #comment 수강권추가, 수강권수정, 일정 추가

--- pters_project/static/user/js/trainer/popup/pters.trainer.popup.ticket_view.js
+++ pters_project/static/user/js/trainer/popup/pters.trainer.popup.ticket_view.js
@@ -301,7 +301,9 @@ class Ticket_view{
 
             let popup_style = $root_content.width() > 650 ? POPUP_FROM_BOTTOM : POPUP_FROM_RIGHT;
             layer_popup.open_layer_popup(POPUP_BASIC, POPUP_ADDRESS_LECTURE_SELECT, 100, popup_style, null, ()=>{
-                lecture_select = new LectureSelector('#wrapper_box_lecture_select', this, 999, {'title':'수업'}, (set_data)=>{
+                let appendix = {'title':'수업', lecture_id:this.data.lecture_id, lecture_name:this.data.lecture_name, lecture_state_cd:this.data.lecture_state_cd,
+                                max:this.data.lecture_max, type_cd:this.data.lecture_type_cd, color:this.data.lecture_color};
+                lecture_select = new LectureSelector('#wrapper_box_lecture_select', this, 999, appendix, (set_data)=>{
                     this.lecture = set_data; //타겟에 선택된 데이터를 set
                     // this.send_data(); wait_here_testing
                     this.if_user_changed_any_information = true;
