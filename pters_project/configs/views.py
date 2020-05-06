6bf3e434c1e939985309b7ad35f682d9788c07b1
hkkim05 <hkkim@spooner.co.kr>
2020-05-06 13:58:51 +0900

    PTERS-2268 #TEST #comment 앱 커스터마이징 테스트

--- pters_project/configs/views.py
+++ pters_project/configs/views.py
@@ -40,6 +40,12 @@ def index(request):
     request.session['PTERS_NAVER_ID_LOGIN_CLIENT_SECRET'] = settings.PTERS_NAVER_ID_LOGIN_CLIENT_SECRET
     # request.session['device_info'] = 'web'
     request.session['setting_theme'] = 'light'
+    domain_url = request.get_full_path().split('/')[0].split('pters.co.kr')[0]
+
+    if domain_url != 'pters':
+        request.session['domain_url'] = domain_url
+    else:
+        request.session['domain_url'] = 'pters'
     if request.user.is_authenticated():
         next_page = '/check/'
     else:
