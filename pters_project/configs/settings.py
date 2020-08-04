e0befe4bc4bcac57dd7e43070ee90a553f57f2a0
hkkim05 <hkkim@spooner.co.kr>
2020-08-04 12:30:07 +0900

    PTERS-2287 #comment key color 설정 추가 - 모바일 하단

--- pters_project/configs/settings.py
+++ pters_project/configs/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pters-test',
+        'NAME': 'pters',
         'USER': os.environ.get("PTERS_DB_USER", ''),
         'PASSWORD': os.environ.get("PTERS_DB_PASSWORD", ''),
         'HOST': os.environ.get("PTERS_DB_HOST", ''),
